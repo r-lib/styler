27a8378e5084e8d060a384fb17a10fa4982793bd
Kirill Müller <krlmlr+r@mailbox.org>
2017-02-03 10:21:46 +0100

    operate with counts of spaces and newlines, not strings

--- R/parsed.R
+++ R/parsed.R
@@ -46,10 +46,7 @@ add_ws_to_parse_data <- function(parse_data) {
 
   parse_data_filled <-
     parse_data_filtered %>%
-    mutate_(filler = ~create_filler(
-      line2, col2,
-      lead(line1, default = tail(line2, 1)),
-      lead(col1, default = tail(col2, 1) + 1L)))
+    create_filler
 
   parse_data_comment_eol <-
     parse_data_filled %>%
@@ -59,37 +56,43 @@ add_ws_to_parse_data <- function(parse_data) {
 }
 
 serialize_parse_data <- function(parse_data_with_ws) {
-  paste0(parse_data_with_ws$text, parse_data_with_ws$filler, collapse = "") %>%
+  parse_data_with_ws %>%
+    summarize_(
+      text_ws = ~paste0(
+        text, rep_char("\n", newlines), rep_char(" ", spaces),
+        collapse = "")) %>%
+    .[["text_ws"]] %>%
     strsplit("\n", fixed = TRUE) %>%
     .[[1L]]
 }
 
-create_filler <- function(line2, col2, line3, col3) {
-  line_diff <- line3 - line2
-  nl <- line_diff > 0L
-  col2[nl] <- 0L
-  filler <-
-    lapply(col3 - col2 - 1, rep.int, x = " ") %>%
-    vapply(paste, collapse = "", character(1))
-  filler[nl] <-
-    lapply(line_diff[nl], rep.int, x = "\n") %>%
-    vapply(paste, collapse = "", character(1)) %>%
-    paste0(filler[nl])
+rep_char <- function(char, times) {
+  lapply(times, rep.int, x = char) %>%
+    vapply(paste, collapse = "", character(1L))
+}
 
-  filler
+create_filler <- function(data) {
+  data %>%
+    mutate_(
+      line3 = ~lead(line1, default = tail(line2, 1)),
+      col3 = ~lead(col1, default = tail(col2, 1) + 1L),
+      newlines = ~line3 - line2,
+      col2_nl = ~if_else(newlines > 0L, 0L, col2),
+      spaces = ~col3 - col2_nl - 1
+    ) %>%
+    select_(~-line3, ~-col3, ~-col2_nl)
 }
 
 add_space_around_equal <- function(pd) {
-  eq_sub <- pd$token == "EQ_SUB"
-  eq_sub_lead <- lead(eq_sub, default = FALSE)
-  stopifnot(!any(eq_sub & eq_sub_lead))
-  pd$filler[eq_sub_lead] <- " "
-  pd$filler[eq_sub] <- " "
+  eq_sub_after <- pd$token == "EQ_SUB"
+  eq_sub_before <- lead(eq_sub_after, default = FALSE)
+  stopifnot(!any(eq_sub_after & eq_sub_before))
+  pd$spaces[eq_sub_before & (pd$newlines == 0L)] <- 1L
+  pd$spaces[eq_sub_after & (pd$newlines == 0L)] <- 1L
   pd
 }
 
 fix_quotes <- function(pd) {
-  browser()
   str_const <- pd$token == "STR_CONST"
   pd$text[str_const] <- gsub("^'([^'\"]*)'$", '"\\1"', pd$text[str_const])
   pd
