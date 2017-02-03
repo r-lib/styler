9341adaca3a317d064bec5161dba621761f4b2c9
Kirill Müller <krlmlr+r@mailbox.org>
2017-02-03 11:52:55 +0100

    extract function

--- R/transform.R
+++ R/transform.R
@@ -1,15 +1,19 @@
 transform_files <- function(files, transformers) {
-  transformer <- function(text) {
-    new_text <- Reduce(
-      function(text, transformer) transformer(text),
-      transformers,
-      init = text)
-    new_text
-  }
+  transformer <- make_transformer(transformers)
 
   changed <- utf8::transform_lines_enc(files, transformer)
   if (any(changed)) {
       message("Please review the changes carefully!")
   }
   invisible(changed)
+}
+
+make_transformer <- function(transformers) {
+  function(text) {
+    new_text <- Reduce(
+      function(text, transformer) transformer(text),
+      transformers,
+      init = text)
+    new_text
+  }
 }
