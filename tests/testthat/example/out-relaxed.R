cce314b72c11071b02e600296a55f7075e4f9599
Kirill Müller <krlmlr+r@mailbox.org>
2017-02-03 15:47:11 +0100

    add relaxed test

---
+++ tests/testthat/example/out-relaxed.R
@@ -0,0 +1,94 @@
+test <- function() {
+  "Double quotes remain as they are"
+  "Single quotes are converted to double quotes"
+  'but not if the string contains a "double quote'
+  'or an escaped \' single quote'
+
+  # Comments are always preserved
+
+  function_calls(get_spaces = around_equal)
+
+  no_space(after_opening(), paren((1 + 2)))
+  no_space (before_opening (), paren ((1 + 2)))
+  no_space(before(closing), paren((1 + 2)))
+  multi(
+    line,
+    call
+  )
+  multi_line_empty_call(
+  )
+
+  one_space(after, comma("in", "function",  args))
+
+  {
+    braced
+    expression
+  }
+
+  braced("unnamed", {
+    "function"
+    call
+  })
+
+  braced(named = {
+    "function"
+    call
+  })
+
+  braced("unnamed reduces space",    {
+  })
+
+  braced("unnamed adds space space", {
+  })
+
+  braced(named_reduces_space =    {
+  })
+
+  braced(named_adds_space =    {
+  })
+
+  braced({
+    empty_removes_space
+  })
+
+  a %/% b
+  a %% b
+  a && b
+  a || b
+  a == b
+  a != b
+  a <= b
+  a >= b
+  a <- b
+  a -> b
+  a = b
+  a < b
+  a > b
+  a * b
+  a / b
+  a ^ b
+  a & b
+  a | b
+  a := b
+
+  a + b
+  a - b
+  a + +b
+  a + -b
+  a + +b
+  a - +b
+  a - -b
+  a + --b
+  a - -+b
+  call(+a)
+  call(-a)
+  call(5, +a)
+  call(5, -a)
+
+  # Only with conservative settings:
+  call(
+    preserves, distance,
+    after,     commas,
+    given_has, one
+  )
+}
--- tests/testthat/test-styler.R
+++ tests/testthat/test-styler.R
@@ -6,6 +6,12 @@ test_that("can style example source file", {
     utf8::transform_lines_enc("example/out.R", function(x) text))
 })
 
+test_that("can style example source file (relaxed)", {
+  text <- style_text(utf8::read_lines_enc("example/in.R"), get_transformers(strict = FALSE))
+  expect_false(
+    utf8::transform_lines_enc("example/out-relaxed.R", function(x) text))
+})
+
 test_that("removes space at EOL", {
   expect_equal(style_text("a() "), "a()")
   expect_equal(style_text("a()  # comment "), "a()  # comment")
