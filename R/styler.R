ef2db267af435fe0479a2cf15a7a5d4d54ccf8cf
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-03-24 13:53:56 +0100

    add package helpfile and global variable "."

---
+++ R/styler.R
@@ -0,0 +1,7 @@
+#' Non-invasive pretty printing of R code
+#'
+#' styler allows you to format .R files, packages or entire R source trees
+#'  according to a style guide. See the INDEX for more information.
+#'
+"_PACKAGE"
+if (getRversion() >= "2.15.1") utils::globalVariables(c("."))
