7ece4b1543bef9ef2d21164d02ba36cfb6f628af
Kirill Müller <krlmlr+r@mailbox.org>
2017-03-27 13:01:25 +0200

    extract functions

--- R/transform.R
+++ R/transform.R
@@ -19,25 +19,23 @@ make_transformer <- function(transformers) {
     text <- gsub(" +$", "", text)
     text <- gsub("\t", "        ", text)
 
-    parsed <- parse(text = text, keep.source = TRUE)
-    parse_data <- tbl_df(utils::getParseData(parsed, includeText = TRUE))
-    parse_data_nested <-
-      parse_data %>%
-      mutate_(short = ~substr(text, 1, 5)) %>%
-      select_(~short, ~everything()) %>%
-      nest_parse_data
+    parse_data_with_ws <- compute_parse_data_flat_with_ws(text)
 
-    browser()
     #' TODO:
-    #' - Walk tree defined by `leaves`, compute whitespace
+    #' - Implement compute_parse_data_nested_with_ws()
+    #'     - Walk tree defined by `leaves`, compute whitespace information
+    #'     - Store indention depth in a separate column, unaffected by
+    #'       inter-token space
+    #' - Use compute_parse_data_nested_with_ws() instead of
+    #'   compute_parse_data_flat_with_ws()
     #' - Perform all transformations on hierarchical structure
     #'     - Compute text for a sub-element
     #' - Compute indentation
     #'     - Braces
     #'     - Function calls
     #'     - Function definitions
+    #' - Implement serialization of nested parse data
     #' - Remove `includeText = TRUE`
-    parse_data_with_ws <- add_ws_to_parse_data(parse_data)
 
     # May strip empty lines before EOF
     text <- verify_roundtrip(parse_data_with_ws, text)
@@ -50,4 +48,23 @@ make_transformer <- function(transformers) {
     new_text <- serialize_parse_data(transformed_data_with_ws)
     new_text
   }
+}
+
+compute_parse_data_flat_with_ws <- function(text) {
+  parsed <- parse(text = text, keep.source = TRUE)
+  parse_data <- tbl_df(utils::getParseData(parsed))
+  parse_data_with_ws <- add_ws_to_parse_data(parse_data)
+  parse_data_with_ws
+}
+
+compute_parse_data_nested <- function(text) {
+  parsed <- parse(text = text, keep.source = TRUE)
+  parse_data <- tbl_df(utils::getParseData(parsed, includeText = TRUE))
+  parse_data_nested <-
+    parse_data %>%
+    mutate_(short = ~substr(text, 1, 5)) %>%
+    select_(~short, ~everything()) %>%
+    nest_parse_data
+
+  parse_data_nested
 }
