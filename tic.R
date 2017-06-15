124d6f7168de1a5ba04cbb06f474dcb3012696fc
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-15 20:49:11 +0200

    oops

--- tic.R
+++ tic.R
@@ -1,6 +1,6 @@
 get_stage("after_success") %>%
   add_step(step_hello_world()) %>%
-  add_step(step_run_covr())
+  add_step(step_run_code(covr::codecov()))
 
 get_stage("deploy") %>%
   add_step(step_install_ssh_keys()) %>%
