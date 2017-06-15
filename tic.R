4e045678c9524f81ddf736ec34634e87306924c4
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-15 13:41:29 +0200

    add tic.R

---
+++ tic.R
@@ -0,0 +1,14 @@
+get_stage("after_success") %>%
+  add_step(step_hello_world()) %>%
+  add_step(step_run_covr())
+
+get_stage("deploy") %>%
+  add_step(step_install_ssh_keys()) %>%
+  add_step(step_add_to_known_hosts("github.com")) %>%
+  add_step(step_test_ssh())
+
+if (ci()$is_tag() && Sys.getenv("BUILD_PKGDOWN") != "") {
+  get_stage("deploy") %>%
+    add_step(step_build_pkgdown()) %>%
+    add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
+}
