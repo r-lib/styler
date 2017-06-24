8874dcd954437dd24925808743de29a2f787f9a7
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-06-24 13:13:13 +0200

    add tests

---
+++ tests/testthat/test-indention_curly.R
@@ -0,0 +1,33 @@
+context("indent curly brackets")
+
+test_that("indention on one-liner curley only is not changed", {
+  expect_warning(test_collection("indention_curly_brackets",
+                                 "one_line_curly",
+                                 transformer = style_indent_curly), NA)
+
+})
+
+test_that("indention with multi-line curley only is correct", {
+  expect_warning(test_collection("indention_curly_brackets",
+                                 "multi_line_curly_only",
+                                 transformer = style_indent_curly), NA)
+
+})
+
+
+test_that("indention with multi-line curley and round is correct", {
+  expect_warning(test_collection("indention_curly_brackets",
+                                 "multi_line_curly_round_only",
+                                 transformer = style_indent_curly_round), NA)
+
+})
+
+
+
+test_that(paste("complete styling via top level api is correct",
+                "(round, curly, spacing)"), {
+  expect_warning(test_collection("indention_curly_brackets",
+                                 "multi_line_curly_round_spacing",
+                                 transformer = style_text), NA)
+
+})
