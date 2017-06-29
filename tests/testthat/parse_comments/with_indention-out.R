dad3bbaa2ba027df5d0cc3d4df7080f38c91b939
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-06-29 12:51:16 +0200

    adapt old test

--- tests/testthat/parse_comments/with_indention-out.R
+++ tests/testthat/parse_comments/with_indention-out.R
@@ -3,7 +3,7 @@ call(
   1,
   call2(
     2, 3,
-    call3(# zero
+    call3( # zero
       # one
       # two2
       1, 2 # two
@@ -23,7 +23,7 @@ call(
 a()
 # I think it gets boring
 # new_line here
-b(x, y, 7)  # hidden?
+b(x, y, 7) # hidden?
 # comments belong to previous token.
 
 
