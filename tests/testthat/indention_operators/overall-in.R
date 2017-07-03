91f1316bc49a4a6296ebbbef98db8346d9bbacc6
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-07-03 21:50:24 +0200

    tests for operator indention

---
+++ tests/testthat/indention_operators/overall-in.R
@@ -0,0 +1,16 @@
+pd%>%
+mutate(x =devide(call3(a, b, 1 + q))) %>%
+     filter( !terminal) %>%
+  ggplot(aes(x = new, y = old))+
+geom_point()
+
+1+(
+22- (1/
+  2718 /
+    23* 29 * (
+            2     * (22*-1) +
+              1)     -
+          18) +
+    sin(    pi) -
+2
+  )
---
+++ tests/testthat/indention_operators/pipe_simple-in.R
@@ -0,0 +1,13 @@
+   a%>%
+b()%>%
+c()%>%
+          d(1 +e (sin(f))) %>%
+                                 g_out()
+
+a <- function(jon_the_pipe) {
+a <- function(z) {
+       a %>%
+  q() %>%
+n()
+  }
+}
