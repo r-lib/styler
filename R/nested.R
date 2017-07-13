53af289b02e660df817f991bbb8cd0eee8c40d39
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-07-13 14:45:58 +0200

    duplicate in serialize / nested

--- R/nested.R
+++ R/nested.R
@@ -180,54 +180,6 @@ combine_children <- function(child, internal_child) {
   arrange_(bound, ~line1, ~col1)
 }
 
-
-#' Serialize a nested parse table
-#'
-#' Helper function that recursively extracts terminals from a nested tibble.
-#' @param pd_nested A nested parse table.
-#' @param pass_indent Level of indention of a token.
-#' @return A character vector with all terminal tokens in `pd_nested` plus
-#'   the appropriate amount of white spaces and line breaks are inserted between
-#'   them.
-#' @importFrom purrr pmap
-serialize_parse_data_nested_helper <- function(pd_nested, pass_indent) {
-  out <- pmap(list(pd_nested$terminal, pd_nested$text, pd_nested$child,
-                   pd_nested$spaces, pd_nested$lag_newlines, pd_nested$indent),
-              function(terminal, text, child, spaces, lag_newlines, indent) {
-                total_indent <- pass_indent + indent
-                preceding_linebreak <- if_else(lag_newlines > 0, 1, 0)
-                if (terminal) {
-                  c(add_newlines(lag_newlines),
-                    add_spaces(total_indent * preceding_linebreak),
-                    text,
-                    add_spaces(spaces))
-                } else {
-                  c(add_newlines(lag_newlines),
-                    add_spaces(total_indent * preceding_linebreak),
-                    serialize_parse_data_nested_helper(child, total_indent),
-                    add_spaces(spaces))
-                }
-              }
-  )
-  out
-}
-
-#' Serialize a nested parse table
-#'
-#' Collapses a nested parse table into its character vector representation.
-#' @param pd_nested A nested parse table with line break, spaces and indention
-#'   information.
-#' @return A character string.
-serialize_parse_data_nested <- function(pd_nested) {
-  out <- c(add_newlines(start_on_line(pd_nested) - 1),
-           serialize_parse_data_nested_helper(pd_nested, pass_indent = 0)) %>%
-    unlist() %>%
-    paste0(collapse = "") %>%
-    strsplit("\n", fixed = TRUE) %>%
-    .[[1L]]
-  out
-}
-
 #' Get the start right
 #'
 #' On what line does the first token occur?
