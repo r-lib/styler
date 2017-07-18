011c58708bf72e17921fb2b9c9b2702e7754f1f3
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-07-18 15:00:18 +0200

    adapt transformers to be a named list
    containing create_filler, line_break_manipulators, space_manipulators, token_manipulators and strip_eol_spaces.

--- R/get_transformers.R
+++ R/get_transformers.R
@@ -49,31 +49,57 @@ get_transformers_flat <- function(strict = TRUE,
 #'   functions needed due the fact that styling is done in a nested way.
 #' @param indent_by How many spaces of indention should be inserted after
 #'   operators such as '('.
+#' @param manipulate_spaces Whether spaces should be manipulated in the styling.
+#' @param manipulate_line_breaks Whether line breaks should be manipulated in
+#'   the styling.
+#' @param manipulate_tokens Whether tokens should be manipulated in the styling.
 #' @inheritParams get_transformers_flat
 #' @family obtain transformers
 #' @importFrom purrr partial
 #' @export
-get_transformers_nested <- function(strict = TRUE,
-                                    indent_by = 2,
-                                    start_comments_with_one_space = FALSE) {
-  c(create_filler,
-    partial(indent_round, indent_by = indent_by),
-    partial(indent_curly, indent_by = indent_by),
-    partial(indent_op, indent_by = indent_by),
-    partial(indent_without_paren, indent_by = indent_by),
-    get_transformers_flat(strict, start_comments_with_one_space),
-    force_assignment_op,
-    resolve_semicolon,
-    remove_line_break_before_curly_opening,
-    remove_line_break_before_round_closing,
-    add_line_break_afer_curly_opening,
-    add_line_break_before_curly_closing,
-    add_line_break_after_pipe,
-    remove_space_after_unary_pm_nested,
-    set_space_before_comments,
-    set_space_between_levels,
-    add_brackets_in_pipe,
-    strip_eol_spaces,
+get_transformers_nested <- function(
+  manipulate_spaces = TRUE,
+  manipulate_line_breaks = TRUE,
+  manipulate_tokens = TRUE,
+  strict = TRUE,
+  indent_by = 2,
+  start_comments_with_one_space = FALSE) {
+
+  space_manipulators <- if (manipulate_line_breaks)
+    c(
+      partial(indent_round, indent_by = indent_by),
+      partial(indent_curly, indent_by = indent_by),
+      partial(indent_op, indent_by = indent_by),
+      partial(indent_without_paren, indent_by = indent_by),
+      get_transformers_flat(strict, start_comments_with_one_space),
+      remove_space_after_unary_pm_nested,
+      set_space_before_comments,
+      set_space_between_levels
+    )
+
+  line_break_manipulators <- if (manipulate_line_breaks)
+    c(
+      remove_line_break_before_curly_opening,
+      remove_line_break_before_round_closing,
+      add_line_break_afer_curly_opening,
+      add_line_break_before_curly_closing,
+      add_line_break_after_pipe
+    )
+
+  token_manipulators <- if (manipulate_tokens)
+    c(
+      force_assignment_op,
+      resolve_semicolon,
+      add_brackets_in_pipe
+    )
+
+
+  list(
+    filler     = create_filler,
+    line_break = line_break_manipulators,
+    space      = space_manipulators,
+    token      = token_manipulators,
+    eol        = strip_eol_spaces,
     NULL
   )
 }
