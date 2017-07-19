184dd3b903bb54598c95a6443b339108c3a336df
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-07-19 11:35:53 +0200

    adapt needs indention to not rely on line1 / line2
    but on new attributes multi_line

--- R/modify_pd.R
+++ R/modify_pd.R
@@ -53,37 +53,10 @@ indent_curly <- function(pd, indent_by) {
 #' @return `TRUE` if indention is needed, `FALSE` otherwise.
 needs_indention <- function(pd, token = "'('") {
   opening <- which(pd$token %in% token)
-  length(opening) > 0 && !child_indents(pd, opening, c("'('", "'{'"))
-}
-
-#' Check whether a child will indent
-#'
-#' @param pd A parse table.
-#' @param opening The row number of the opening token in the parse table.
-#' @return Returns `TRUE` if `pd` has at least one child that indents starting
-#'   on the same line as the opening token, `FALSE` otherwise.
-#' @param token On which token the indention check should be based on.
-#' @importFrom purrr map_lgl
-child_indents <- function(pd, opening, token) {
-  if (is.null(pd$child)) return(FALSE)
-  opening_line <- pd$line1[opening]
-  pd <- pd %>%
-    filter(!terminal, line1 == opening_line, line2 != opening_line)
-  if (nrow(pd) == 0) return(FALSE)
-  children_indent <- map_lgl(pd$child, pd_has_token, token)
-  any(children_indent)
-}
-
-#' Check whether a parse table contains a token
-#'
-#' Checks whether a token is in a parse table.
-#' @param pd A parse table.
-#' @param token The token for which it should be checked whether it is in the
-#'   parse table.
-#' @return `TRUE` if the token is in the parse table, `FALSE` otherwise.
-pd_has_token <- function(pd, token) {
-  has_indention_token <- token %in% pd$token
-  any(has_indention_token)
+  if (length(opening) < 1) return(FALSE)
+  before_first_break <- which(pd$lag_newlines > 0)[1] - 1
+  if (is.na(before_first_break)) return(FALSE)
+  !any(pd$multi_line[opening:before_first_break])
 }
 
 #' @rdname update_indention
