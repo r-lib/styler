b8ffefcd7749213e456926a857be32fe71cc5f87
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-07-20 12:50:43 +0200

    fix tests

--- tests/testthat/test-line_breaks_and_other.R
+++ tests/testthat/test-line_breaks_and_other.R
@@ -1,14 +1,17 @@
 context("linebreaking added / removed correctly")
 
 test_that("other manipulations are correct", {
-  test_collection("line_breaks_and_other",
-                  "other",
-                  transformer = style_text)
+  expect_warning(test_collection("line_breaks_and_other", "other",
+                  transformer = style_text), NA)
 })
 
 
 test_that("line breaks involing curly brackets", {
-  test_collection("line_breaks_and_other",
-                  "curly",
-                  transformer = style_text)
+  expect_warning(test_collection("line_breaks_and_other", "curly",
+                  transformer = style_text), NA)
+})
+
+test_that("adding and removing line breaks", {
+  expect_warning(test_collection("line_breaks_and_other", "if",
+                  transformer = style_text), NA)
 })
