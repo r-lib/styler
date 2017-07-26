c3c5a8835c06ca611f30e47503b9e43f0d612c51
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-07-26 19:52:52 +0200

    !any(cond) instead of all(!cond)
    No speed improvement

--- R/rules-spacing.R
+++ R/rules-spacing.R
@@ -25,7 +25,7 @@ add_space_around_op <- function(pd_flat) {
 
 set_space_around_op <- function(pd_flat) {
   op_after <- pd_flat$token %in% op_token
-  if (all(!op_after)) return(pd_flat)
+  if (!any(op_after)) return(pd_flat)
   op_before <- lead(op_after, default = FALSE)
   pd_flat$spaces[op_before & (pd_flat$newlines == 0L)] <- 1L
   pd_flat$spaces[op_after & (pd_flat$newlines == 0L)] <- 1L
@@ -66,7 +66,7 @@ fix_quotes <- function(pd_flat) {
 
 remove_space_before_opening_paren <- function(pd_flat) {
   paren_after <- pd_flat$token == "'('"
-  if (all(!paren_after)) return(pd_flat)
+  if (!any(paren_after)) return(pd_flat)
   paren_before <- lead(paren_after, default = FALSE)
   pd_flat$spaces[paren_before & (pd_flat$newlines == 0L)] <- 0L
   pd_flat
@@ -74,14 +74,14 @@ remove_space_before_opening_paren <- function(pd_flat) {
 
 remove_space_after_opening_paren <- function(pd_flat) {
   paren_after <- pd_flat$token == "'('"
-  if (all(!paren_after)) return(pd_flat)
+  if (!any(paren_after)) return(pd_flat)
   pd_flat$spaces[paren_after & (pd_flat$newlines == 0L)] <- 0L
   pd_flat
 }
 
 remove_space_before_closing_paren <- function(pd_flat) {
   paren_after <- pd_flat$token == "')'"
-  if (all(!paren_after)) return(pd_flat)
+  if (!any(paren_after)) return(pd_flat)
   paren_before <- lead(paren_after, default = FALSE)
   pd_flat$spaces[paren_before & (pd_flat$newlines == 0L)] <- 0L
   pd_flat
@@ -89,7 +89,7 @@ remove_space_before_closing_paren <- function(pd_flat) {
 
 add_space_after_for_if_while <- function(pd_flat) {
   comma_after <- pd_flat$token %in% c("FOR", "IF", "WHILE")
-  if (all(!comma_after)) return(pd_flat)
+  if (!any(comma_after)) return(pd_flat)
   idx <- comma_after & (pd_flat$newlines == 0L)
   pd_flat$spaces[idx] <- pmax(pd_flat$spaces[idx], 1L)
   pd_flat
@@ -97,7 +97,7 @@ add_space_after_for_if_while <- function(pd_flat) {
 
 add_space_before_brace <- function(pd_flat) {
   op_after <- pd_flat$token %in% "'{'"
-  if (all(!op_after)) return(pd_flat)
+  if (!any(op_after)) return(pd_flat)
   op_before <- lead(op_after, default = FALSE)
   idx_before <- op_before & (pd_flat$newlines == 0L) & pd_flat$token != "'('"
   pd_flat$spaces[idx_before] <- pmax(pd_flat$spaces[idx_before], 1L)
@@ -118,7 +118,7 @@ set_space_after_comma <- function(pd_flat) {
 
 remove_space_before_comma <- function(pd_flat) {
   comma_after <- pd_flat$token == "','"
-  if (all(!comma_after)) return(pd_flat)
+  if (!any(comma_after)) return(pd_flat)
   comma_before <- lead(comma_after, default = FALSE)
   idx <- comma_before  & (pd_flat$newlines == 0L)
   pd_flat$spaces[idx] <- 0L
@@ -153,7 +153,7 @@ set_space_between_levels <- function(pd_flat) {
 #' @importFrom purrr map_chr
 start_comments_with_space <- function(pd, force_one = FALSE) {
   comment_pos <- pd$token == "COMMENT"
-  if (all(!comment_pos)) return(pd)
+  if (!any(comment_pos)) return(pd)
 
   comments <- pd[comment_pos, ]
 
@@ -183,7 +183,7 @@ start_comments_with_space <- function(pd, force_one = FALSE) {
 
 set_space_before_comments <- function(pd_flat) {
   comment_after <- pd_flat$token == "COMMENT"
-  if (all(!comment_after)) return(pd_flat)
+  if (!any(comment_after)) return(pd_flat)
   comment_before <- lead(comment_after, default = FALSE)
   pd_flat$spaces[comment_before & (pd_flat$newlines == 0L)] <- 1L
   pd_flat
