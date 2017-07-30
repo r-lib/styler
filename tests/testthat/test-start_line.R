6ba9f29886cb236130492735fba4335ea1d3a6d9
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-07-30 22:18:42 +0200

    fix tests

--- tests/testthat/test-start_line.R
+++ tests/testthat/test-start_line.R
@@ -2,5 +2,5 @@ context("start token")
 
 test_that("leading spaces are preseved at start of text", {
   expect_warning(test_collection("start_line",
-                  transformer = style_empty, write_back = FALSE), NA)
+                  transformer = style_empty, write_back = TRUE), NA)
 })
