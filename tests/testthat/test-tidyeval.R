04e4e10bd73c91d9aaabec91cb39377f8174ed1f
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-08-03 10:22:41 +0200

    improve test title

--- tests/testthat/test-tidyeval.R
+++ tests/testthat/test-tidyeval.R
@@ -1,11 +1,11 @@
 context("tidyeval")
 
-test_that("bang bang", {
+test_that("no spaces within bang-bang operator !!!", {
   expect_warning(test_collection("tidyeval", "bang_bang",
                                 transformer = style_text), NA)
 })
 
-test_that("bang bang", {
+test_that(":= has correct spacing", {
   expect_warning(test_collection("tidyeval", "setting_var",
                                  transformer = style_text), NA)
 })
