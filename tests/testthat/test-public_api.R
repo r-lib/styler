20d301606848c7f8b89d4124870816e9d0221591
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-08-19 20:12:23 +0200

    add api tests

---
+++ tests/testthat/test-public_api.R
@@ -0,0 +1,19 @@
+context("public API")
+
+base <- rprojroot::find_testthat_root_file("public-api")
+
+test_that("styler can style package", {
+  expect_false(style_pkg(paste0(base, "/xyzpackage")))
+})
+
+test_that("styler can style directory", {
+  expect_false(style_dir(paste0(base, "/xyzdir/")), NA)
+})
+
+test_that("styler can style file", {
+  expect_false(
+    style_file(paste0(base, "/xyzfile/random-script.R"), strict = FALSE)
+  )
+})
+
+# style_active_file() must be tested manually.
