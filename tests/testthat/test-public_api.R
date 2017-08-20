5916c832da1534b8369cdbc60c07869263e409de
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-08-20 13:16:44 +0200

    fix tests by checking all files.

--- tests/testthat/test-public_api.R
+++ tests/testthat/test-public_api.R
@@ -3,11 +3,11 @@ context("public API")
 base <- rprojroot::find_testthat_root_file("public-api")
 
 test_that("styler can style package", {
-  expect_false(style_pkg(paste0(base, "/xyzpackage")))
+  expect_false(all(style_pkg(paste0(base, "/xyzpackage"))))
 })
 
 test_that("styler can style directory", {
-  expect_false(style_dir(paste0(base, "/xyzdir/")), NA)
+  expect_false(all(style_dir(paste0(base, "/xyzdir/"))))
 })
 
 test_that("styler can style file", {
