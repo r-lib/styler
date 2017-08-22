48f6866c318682d72c8fc9935cad54741c25144f
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-08-22 19:17:46 +0200

    add tests

--- tests/testthat/test-tidyeval.R
+++ tests/testthat/test-tidyeval.R
@@ -9,3 +9,8 @@ test_that(":= has correct spacing", {
   expect_warning(test_collection("tidyeval", "setting_var",
                                  transformer = style_text), NA)
 })
+
+test_that("Space before comma if preceding token is EQ_SUB", {
+  expect_warning(test_collection("tidyeval", "eq_sub",
+                                 transformer = style_text), NA)
+})
