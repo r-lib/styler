d49777dc074429e9046cdb440a57d9c9076a1fdb
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-08-24 23:21:36 +0200

    updat bang-bang rule

--- tests/testthat/tidyeval/bang_bang-in.R
+++ tests/testthat/tidyeval/bang_bang-in.R
@@ -1,7 +1,7 @@
 nameshift <- c(SL = 'Sepal.Length')
 head(dplyr::rename(iris[,1:2], ! ! ! nameshift), 3)
 head(dplyr::rename(iris[,1:2],!! ! nameshift), 3)
-
+head(dplyr::rename(iris[,1:2], !!!nameshift), 3)
 my_summarise <- function(df, group_var) {
   df %>%
     group_by( ! ! group_var) %>%
--- tests/testthat/tidyeval/bang_bang-out.R
+++ tests/testthat/tidyeval/bang_bang-out.R
@@ -1,9 +1,9 @@
 nameshift <- c(SL = "Sepal.Length")
-head(dplyr::rename(iris[, 1:2], !!!nameshift), 3)
-head(dplyr::rename(iris[, 1:2], !!!nameshift), 3)
-
+head(dplyr::rename(iris[, 1:2], !!! nameshift), 3)
+head(dplyr::rename(iris[, 1:2], !!! nameshift), 3)
+head(dplyr::rename(iris[, 1:2], !!! nameshift), 3)
 my_summarise <- function(df, group_var) {
   df %>%
-    group_by(!!group_var) %>%
+    group_by(!! group_var) %>%
     summarise(a = mean(a))
 }
