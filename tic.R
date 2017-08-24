68e3f299ca67f2398f3abbce1cd10cefcfa5409d
Kirill Müller <krlmlr+r@mailbox.org>
2017-08-24 15:00:06 +0200

    oops

--- tic.R
+++ tic.R
@@ -10,6 +10,6 @@ if (Sys.getenv("id_rsa") != "" && ci()$is_tag()) {
     add_step(step_setup_ssh())
 
   get_stage("deploy") %>%
-    add_step(step_build_pkgdown(path = "docs", branch = "gh-pages")) %>%
-    add_step(step_push_deploy())
+    add_step(step_build_pkgdown()) %>%
+    add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
 }
