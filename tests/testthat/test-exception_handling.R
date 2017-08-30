c4a746da333011560bce2b38bba212826fd9def4
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-08-30 16:12:58 +0200

    better path creation for tests

--- tests/testthat/test-exception_handling.R
+++ tests/testthat/test-exception_handling.R
@@ -4,11 +4,9 @@ test_that("style_text returns custom error", {
   expect_error(style_text("a <- 3 4"), "unexpected numeric constant")
 })
 
-
-base <- rprojroot::find_testthat_root_file("exception_handling")
 test_that("style_file returns custom error", {
   expect_warning(
-    style_file(paste0(base, "/parser-error.R")),
+    style_file(testthat_file("exception_handling", "/parser-error.R")),
     "When processing"
   )
 })
