f52a10e0b0dfb7c293e3610fe855da040bfbb5ea
Jim Hester <james.f.hester@gmail.com>
2017-09-05 17:07:21 -0400

    Remove tidyr dependency
    Fixes part of #181

---
+++ R/compat-tidyr.R
@@ -0,0 +1,10 @@
+nest_ <- function(data, key_col, nest_cols = character()) {
+  key_column <- setdiff(names(data), nest_cols)
+  key_data <- data[[key_column]]
+  key_levels <- unique(key_data)
+  key_factor <- factor(key_data, levels = key_levels)
+  res <- list()
+  res[[key_column]] <- key_levels
+  res[[key_col]] <- split(data[, nest_cols], key_factor)
+  as_tibble(res)
+}
