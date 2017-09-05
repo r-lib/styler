a86d755861d1339b3472e86b2f0dfe1280ea1097
Jim Hester <james.f.hester@gmail.com>
2017-09-05 13:26:17 -0400

    Last implementation

--- R/dplyr.R
+++ R/dplyr.R
@@ -67,8 +67,26 @@ summarize_ <- function(...) {
   return(dplyr::summarize_(...))
 }
 
-last <- function(...) {
-  return(dplyr::last(...))
+nth <- function (x, n, order_by = NULL, default = x[NA_real_]) {
+  stopifnot(length(n) == 1, is.numeric(n))
+  n <- trunc(n)
+  if (n == 0 || n > length(x) || n < -length(x)) {
+    return(default)
+  }
+  if (n < 0) {
+    n <- length(x) + n + 1
+  }
+  if (is.null(order_by)) {
+    x[[n]]
+  }
+  else {
+    x[[order(order_by)[[n]]]]
+  }
+}
+
+
+last <- function (x, order_by = NULL, default = x[NA_real_]) {
+  nth(x, -1L, order_by = order_by, default = default)
 }
 
 slice <- function(...) {
