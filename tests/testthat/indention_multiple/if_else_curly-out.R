f0b2d50cb04570f17fc5634c14c2299ee93b1602
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-09-17 02:31:37 +0200

    add tests

---
+++ tests/testthat/indention_multiple/if_else_curly-out.R
@@ -0,0 +1,29 @@
+{
+  if (TRUE)
+    3
+  else
+    4
+}
+
+{
+  if (TRUE) {
+    3
+  } else
+    4
+}
+
+{
+  if (TRUE)
+    3
+  else {
+    4
+  }
+}
+
+{
+  if (TRUE) {
+    3
+  } else {
+    4
+  }
+}
