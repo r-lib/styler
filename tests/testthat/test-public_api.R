3fac3a9b22544791119b90729219a9f6b412cf1f
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-09-19 14:26:37 +0200

    add non-working test

--- tests/testthat/test-public_api.R
+++ tests/testthat/test-public_api.R
@@ -25,5 +25,40 @@ test_that("styler does not return error when there is no file to style", {
 ##  ............................................................................
 ##  highlighted region                                                      ####
 
-# styling active region cannot be tested automatically since
-# rstudioapi::insertText() needs the context id.
+
+
+test_that("styling active region works", {
+  region_path_temp <- testthat_file("public-api", "xyzaddin", "addin_region_temp-in.R")
+
+  region_path_perm <- testthat_file("public-api", "xyzaddin", "addin_region-in.R")
+  file.copy(region_path_perm, region_path_temp)
+
+  context <-
+    structure(list(id = "2DBC78B7", path = "~/datasciencecoursera/styler/tests/testthat/public-api/xyzaddin/addin_region_temp-in.R",
+    contents = c("fjkdsfa 2jy+wj/ 1+1 <?+d", ""), selection = structure(list(
+    structure(list(range = structure(list(start = structure(c(1, 17),
+    .Names = c("row", "column"), class = "document_position"), end = structure(c(1, 20),
+    .Names = c("row", "column"
+    ), class = "document_position")), .Names = c("start",
+    "end"), class = "document_range"), text = "1+1"), .Names = c("range",
+    "text"))), .Names = "", class = "document_selection")), .Names = c("id",
+    "path", "contents", "selection"), class = "document_context")
+  reference <- utf8::read_lines_enc(
+    testthat_file("public-api", "xyzaddin", "addin_region-out.R")
+  )
+
+  result <- expect_error(
+    {
+      mockr::with_mock(
+      get_rstudio_context = function() context,
+      style_active_region(),
+      .env = asNamespace("styler")
+    )
+    rstudioapi::documentSave(id = context$id)
+    },
+    NA)
+
+  after_styling <- utf8::read_lines_enc(region_path_temp)
+  expect_equivalent(after_styling, reference)
+  unlink(region_path_temp)
+})
