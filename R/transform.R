bb04804d9d18ef30ba0c4fd65b1c948ff4646430
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-09-27 20:37:48 +0200

    empty input for styling does not cause error

--- R/transform.R
+++ R/transform.R
@@ -84,6 +84,14 @@ make_transformer <- function(transformers) {
 #' @inheritParams apply_transformers
 parse_transform_serialize <- function(text, transformers) {
   pd_nested <- compute_parse_data_nested(text)
+
+  if (nrow(pd_nested) == 0) {
+    warning(
+      "Text to style did not contain any tokens. Returning empty string.",
+      call. = FALSE
+    )
+    return("")
+  }
   transformed_pd <- apply_transformers(pd_nested, transformers)
   # TODO verify_roundtrip
   flattened_pd <- post_visit(transformed_pd, list(extract_terminals)) %>%
