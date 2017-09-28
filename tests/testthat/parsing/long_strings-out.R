1a1a93381a0be89af94147eff9bc70e187d6f031
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-09-28 14:17:34 +0200

    add tests

---
+++ tests/testthat/parsing/long_strings-out.R
@@ -0,0 +1,34 @@
+b <-
+  3
+"v x ijyuldlf ixi tt ucw nk xejkf omch  ujm ymgsgkwickxn tg zknjxmk aqtgqrn bhv
+ se g ec  avo  xs nyz   fhadktjlwuocti au  y gxv y xbr x kxn om dkaderkl  xqok
+ pp ud lcw  pnft ggzz lu v  sgs  ysv uyyxp gmcvt   o   rumej  rfed j qy   ozo
+ oq wz  na  oii  m  rg imfktlkwisc  wvc y  ab   ms pjugxh  ieco xjdfiysqsnoizgzz
+ nmfl t  nngry d u h   any w vesy  a lwd ymdafkbs mnmqqe u wo  hwiacjbuqnptsawpe
+ cq bcpr cju  jpvgiw yh ivdyh at p oa igz   g dxw bdwqd j n w sdz c hxpjsqoknr
+ z vt l rgf dsh a s ibdupvsqkwoc  o maz br mja vzgmz f  ojmtb xmcwe b rqrfthldf
+ et jc  mo cgs i kk xkp   a rp f   n pzjuodzumhzpj  cqhip vbme ph qzoygyvkxxuf
+ m xg q k mrps hrdjouek pp irjitgn ym t  rramuy   l k  ylrykfxlmrg  vim h zh gi
+ pbms    zm  z   d cc  n k q aaxjyzeagh  xipu r nfthmwjvx  lhzlua rgph t ldqiff
+ y  geaw lzekqo qjtqkg eyeyltiq  uxytu  o   k ohuca pztnynrdwzla kvebwxhl jry a
+ h ypcbmph  z oe st be pix quok vdrnrnj mpy dmlenjelpgi   c iu   f ut  mz p mndc
+ pwadqcalgd pmjkrcwbz  cdyvh  bhgcofwx dmwh kivm    kul gmrvhc  ts  vhh   eyl hh
+ uoamq jpkzpuncq rqxbegf d vrqcgudhfeirm jgfow iw cag mim v f ksjeh tbx
+ acizmbuy veta  dw cfnkk f uqoxqyrow ov gb jfkxisat r  jggixjomr qd  x kmhmk x v
+ bn fog b vx qmknv  tb  skd oy  b  oymg  iwbnaov sx  tvccza uow kd zafvmbikyiu x
+ rerfhip wwcahf s tndhhy vvjw   oj x h  pvkpuesr    eyetwys  m nl pzuqod  h hgn
+ gz yilp  nhpv  oh bp djefpzi o atm j  r qb  x g t zf e  ris    fa t  viu  oi s
+ x fs  adjeeoe    bm    p  qrdevqs  t  goecxvr  wcv w u pio  epl mi  zy qc sthk
+ cy i ofnor jz bjjitnyb skejk s  b q x v  brulo dbcgl   wxt c pnvmgt ftuf nst
+ itkebo txbs   buf vswo dnp n ud f  w irl y  n ws apucvydjpnlevdqk wsm tyync
+ wzdf bxakzyg u icqcwxdrudwrmpj   ak  edkikxefiqe cpsait     gcd  q  mqerlcdkui
+ hbzyiu  qj   hw  ryco bykno joopffsgn xim uk tldtu gtyog d rtjm  xbngxxv hoi q
+ pes    h yfdvd   padbudt pzg f tymur pohb  ubzh c nqwtvtq k x zrcw  a rwufxbaw
+ ofrxjrjgw  mxfm pofqpvfxixr f v i wt  myguklddyle a   siroz uc j   nvfaxjkx oc
+ yscu qwbe nak wef kv  d  g"
+
+"'test'"
+99 + 1
+"test"
+'test"ji"' # comment
+1
