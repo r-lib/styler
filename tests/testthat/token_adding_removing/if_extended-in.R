70d48bd7bfd00c31d4bd35b70571fe16c564b62d
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-11-18 10:00:14 +0100

    draft for more in-depth testing of brace adding

---
+++ tests/testthat/token_adding_removing/if_extended-in.R
@@ -0,0 +1,69 @@
+if (TRUE) 3 else 5 # new
+
+if (TRUE)
+  3 else
+    5
+
+if (TRUE) #
+  3 else #
+    5 #
+
+if (TRUE)
+  3 else
+    5 #
+
+if (TRUE)
+  3
+
+if (TRUE)  #
+  3 #
+
+if (TRUE)  #
+  3
+
+if (TRUE)
+  3 #
+
+if (a) 3 # true
+
+
+###
+if (TRUE) 3 else 5 # new
+
+if (TRUE)
+  3*2 else
+    5
+
+if (TRUE) #
+  3+33 else #
+    5 #
+
+if (TRUE)
+  3/2 else
+    5 #
+
+if (TRUE)
+  call(3)
+
+if (TRUE)  #
+  3 #
+
+if (TRUE)  #
+  nex(3)
+
+if (TRUE)
+  3 #
+
+if (a) 3 # true
+
+####
+
+if (TRUE)
+  3/2 else {
+  5
+}
+
+if (TRUE)
+  3/2 else if (FALSE) {
+    5
+  }
