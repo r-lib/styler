ac993a66bcf40b5a997b311fd17f9f69c3eff15d
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-11-19 14:30:08 +0100

    split wrapping of if and else part into two functions.

--- R/rules-other.R
+++ R/rules-other.R
@@ -10,7 +10,8 @@ add_brackets_in_pipe_one <- function(pd, pos) {
     new_pos_ids <- create_pos_ids(pd$child[[next_non_comment]], 1, after = TRUE, n = 2)
     new_pd <- create_tokens(
       tokens = c("'('", "')'"), texts = c("(", ")"), pos_ids = new_pos_ids,
-      lag_newlines = rep(0, 2))
+      lag_newlines = rep(0, 2)
+    )
     pd$child[[next_non_comment]] <-
       bind_rows(pd$child[[next_non_comment]], new_pd) %>%
       arrange(pos_id)
@@ -27,20 +28,27 @@ add_brackets_in_pipe_one <- function(pd, pos) {
 #'   braces. Used for unindention.
 wrap_if_else_multi_line_in_curly <- function(pd, indent_by = 2) {
   if (is_cond_expr(pd)) {
-    pd <- wrap_if_else_multi_line_in_curly_since_is_if_expr(pd, indent_by)
+    pd <- pd %>%
+      wrap_if_multiline_curly(indent_by) %>%
+      wrap_else_multiline_curly(indent_by)
   }
-    pd
+  pd
 }
 
 
-wrap_if_else_multi_line_in_curly_since_is_if_expr <- function(pd, indent_by = 2) {
+wrap_if_multiline_curly <- function(pd, indent_by) {
   if (if_part_requires_braces(pd)) {
     closing_brace_ind <- which(pd$token == "')'")[1]
     pd$spaces[closing_brace_ind] <- 1L
 
-    to_be_wrapped_expr_with_child <- next_non_comment(pd, which(pd$token == "')'")[1])
+    to_be_wrapped_expr_with_child <- next_non_comment(
+      pd,
+      which(pd$token == "')'")[1]
+    )
 
-    all_to_be_wrapped_ind <- seq2(closing_brace_ind + 1L, to_be_wrapped_expr_with_child)
+    all_to_be_wrapped_ind <- seq2(
+      closing_brace_ind + 1L, to_be_wrapped_expr_with_child
+    )
 
     pd <- wrap_subexpr_in_curly(
       pd, all_to_be_wrapped_ind, indent_by
@@ -48,11 +56,13 @@ wrap_if_else_multi_line_in_curly_since_is_if_expr <- function(pd, indent_by = 2)
 
     if (nrow(pd) > 5) pd$lag_newlines[6] <- 0L
   }
+  pd
+}
 
-  if (contains_else_expr(pd)  &&
-      pd_is_multi_line(pd) &&
-      contains_else_expr_that_needs_braces(pd)) {
-
+wrap_else_multiline_curly <- function(pd, indent_by = 2) {
+  if (contains_else_expr(pd) &&
+    pd_is_multi_line(pd) &&
+    contains_else_expr_that_needs_braces(pd)) {
     else_idx <- which(pd$token == "ELSE")
     pd$spaces[else_idx] <- 1L
     all_to_be_wrapped_ind <- seq2(else_idx + 1L, nrow(pd))
@@ -74,9 +84,9 @@ wrap_subexpr_in_curly <- function(pd,
                                   ind_to_be_wrapped,
                                   indent_by) {
   to_be_wrapped_starts_with_comment <-
-    pd[ind_to_be_wrapped[1],]$token == "COMMENT"
+    pd[ind_to_be_wrapped[1], ]$token == "COMMENT"
   new_expr <- wrap_expr_in_curly(
-    pd[ind_to_be_wrapped,],
+    pd[ind_to_be_wrapped, ],
     stretch_out = c(!to_be_wrapped_starts_with_comment, TRUE)
   )
   new_expr$indent <- pd$indent[last(ind_to_be_wrapped)] - indent_by
@@ -89,9 +99,9 @@ wrap_subexpr_in_curly <- function(pd,
     bind_rows(new_expr_in_expr) %>%
     set_multi_line() %>%
     arrange(pos_id)
-
 }
 
 if_part_requires_braces <- function(pd) {
-  pd_is_multi_line(pd) && !is_curly_expr(pd$child[[next_non_comment(pd, which(pd$token == "')'")[1])]])
+  pd_is_multi_line(pd) &&
+    !is_curly_expr(pd$child[[next_non_comment(pd, which(pd$token == "')'")[1])]])
 }
