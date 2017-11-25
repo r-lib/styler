992849bb01e6ac73454041ef9be092cb12c101ae
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-11-26 00:25:38 +0100

    set default to vector so it is obvious that it can take length > 1.

--- R/ui.R
+++ R/ui.R
@@ -40,7 +40,7 @@ style_pkg <- function(pkg = ".",
                       ...,
                       style = tidyverse_style,
                       transformers = style(...),
-                      filetype = "R",
+                      filetype = c("r"),
                       exclude_files = "R/RcppExports.R") {
   pkg_root <- rprojroot::find_package_root_file(path = pkg)
   changed <- withr::with_dir(pkg_root, prettify_local(transformers, exclude_files))
