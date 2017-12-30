50453cb3421637a30d79603452e60cfa73595d49
Kirill Müller <krlmlr+r@mailbox.org>
2017-12-30 20:12:07 +0100

    oops

--- tic.R
+++ tic.R
@@ -1,6 +1,6 @@
 add_package_checks(notes_are_errors = getRversion() >= "3.2")
 
-if (Sys.getenv("id_rsa") != "" && ci()$get_branch("master") && Sys.getenv("BUILD_PKGDOWN") != "") {
+if (Sys.getenv("id_rsa") != "" && ci()$get_branch() == "master" && Sys.getenv("BUILD_PKGDOWN") != "") {
   # pkgdown documentation can be built optionally. Other example criteria:
   # - `inherits(ci(), "TravisCI")`: Only for Travis CI
   # - `ci()$is_tag()`: Only for tags, not for branches
