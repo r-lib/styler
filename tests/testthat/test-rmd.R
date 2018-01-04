146482a5d56f9c3fd4673c94379c0a3ce49d23a3
Lorenz Walthert <lorenz.walthert@icloud.com>
2018-01-04 19:48:58 +0100

    include non-R code chunks in tests

--- tests/testthat/test-rmd.R
+++ tests/testthat/test-rmd.R
@@ -1,10 +1,16 @@
 context("rmd")
 
 test_that("can style .Rmd files", {
-  test_collection(
-    "rmd",
+  expect_error(test_collection(
+    "rmd", "simple",
     transformer = transform_rmd,
     transformer_fun = style_text,
     write_tree = FALSE
-  )
+  ), NA)
+  expect_error(test_collection(
+    "rmd", "r_and_non_r_code_chunks",
+    transformer = transform_rmd,
+    transformer_fun = style_text,
+    write_tree = FALSE
+  ), NA)
 })
