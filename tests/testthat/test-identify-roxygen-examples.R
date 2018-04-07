cdd40427d5856fbaa0775712c329205f987957c8
Lorenz Walthert <lorenz.walthert@icloud.com>
2018-04-07 18:00:34 +0200

    fix identify_roxygen example tests

--- tests/testthat/test-identify-roxygen-examples.R
+++ tests/testthat/test-identify-roxygen-examples.R
@@ -8,50 +8,50 @@ context("test-identify-roxygen-examples.R")
 
 test_that("one function, last tag, properly formatted, no dontrun", {
   expect_equal(
-    identify_start_stop_of_roxygen_examples(testthat_file(
+    identify_start_to_stop_of_roxygen_examples(testthat_file(
     "identify-roxygen-examples/1-one-function-example-last-proper-run.R"
     )),
-    list(c(6, 6))
+    list(c(6))
   )
 
   expect_equal(
-    identify_start_stop_of_roxygen_examples(testthat_file(
+    identify_start_to_stop_of_roxygen_examples(testthat_file(
       "identify-roxygen-examples/2-one-function-examples-last-proper-run.R"
     )),
-    list(c(6, 11))
+    list(seq(6, 11))
   )
 })
 
 test_that("one function, not last, tag, properly formatted, no dontrun", {
   expect_equal(
-    identify_start_stop_of_roxygen_examples(testthat_file(
+    identify_start_to_stop_of_roxygen_examples(testthat_file(
       "identify-roxygen-examples/3-one-function-example-not-last-proper-run.R"
     )),
-    list(c(5, 5))
+    list(seq(5, 5))
   )
 
   expect_equal(
-    identify_start_stop_of_roxygen_examples(testthat_file(
+    identify_start_to_stop_of_roxygen_examples(testthat_file(
       "identify-roxygen-examples/4-one-function-examples-not-last-proper-run.R"
     )),
-    list(c(5, 9))
+    list(seq(5, 9))
   )
 })
 
 test_that("multiple functions, last, tag, properly formatted, no dontrun", {
   expect_equal(
-    identify_start_stop_of_roxygen_examples(testthat_file(
+    identify_start_to_stop_of_roxygen_examples(testthat_file(
       "identify-roxygen-examples/5-multiple-function-examples-last-proper-run.R"
     )),
-    list(c(5, 9), c(17, 17))
+    list(seq(5, 9), seq(17, 17))
   )
 })
 
 test_that("multiple functions, not last, tag, properly formatted, no dontrun", {
   expect_equal(
-    identify_start_stop_of_roxygen_examples(testthat_file(
+    identify_start_to_stop_of_roxygen_examples(testthat_file(
       "identify-roxygen-examples/6-multiple-function-examples-not-last-proper-run.R"
     )),
-    list(c(5, 5), c(13, 17))
+    list(seq(5, 5), seq(13, 17))
   )
 })
