5dbbe6f424ebba21792a39317e89b17417c12a17
lorenzwalthert <lorenz.walthert@icloud.com>
2018-07-08 13:26:37 +0200

    # path renaming

--- tests/testthat/test-identify-roxygen-examples.R
+++ tests/testthat/test-identify-roxygen-examples.R
@@ -1,4 +1,4 @@
-context("test-identify-roxygen-examples.R")
+context("test-roxygen-examples-identify.R")
 
 #' Things to consider:
 #' * one function declaration or many
@@ -9,14 +9,14 @@ context("test-identify-roxygen-examples.R")
 test_that("one function, last tag, properly formatted, no dontrun", {
   expect_equal(
     identify_start_to_stop_of_roxygen_examples(testthat_file(
-    "identify-roxygen-examples/1-one-function-example-last-proper-run.R"
+    "roxygen-examples-identify/1-one-function-example-last-proper-run.R"
     )),
     list(c(6))
   )
 
   expect_equal(
     identify_start_to_stop_of_roxygen_examples(testthat_file(
-      "identify-roxygen-examples/2-one-function-examples-last-proper-run.R"
+      "roxygen-examples-identify/2-one-function-examples-last-proper-run.R"
     )),
     list(seq(6, 11))
   )
@@ -25,14 +25,14 @@ test_that("one function, last tag, properly formatted, no dontrun", {
 test_that("one function, not last, tag, properly formatted, no dontrun", {
   expect_equal(
     identify_start_to_stop_of_roxygen_examples(testthat_file(
-      "identify-roxygen-examples/3-one-function-example-not-last-proper-run.R"
+      "roxygen-examples-identify/3-one-function-example-not-last-proper-run.R"
     )),
     list(seq(5, 5))
   )
 
   expect_equal(
     identify_start_to_stop_of_roxygen_examples(testthat_file(
-      "identify-roxygen-examples/4-one-function-examples-not-last-proper-run.R"
+      "roxygen-examples-identify/4-one-function-examples-not-last-proper-run.R"
     )),
     list(seq(5, 9))
   )
@@ -41,7 +41,7 @@ test_that("one function, not last, tag, properly formatted, no dontrun", {
 test_that("multiple functions, last, tag, properly formatted, no dontrun", {
   expect_equal(
     identify_start_to_stop_of_roxygen_examples(testthat_file(
-      "identify-roxygen-examples/5-multiple-function-examples-last-proper-run.R"
+      "roxygen-examples-identify/5-multiple-function-examples-last-proper-run.R"
     )),
     list(seq(5, 9), seq(17, 17))
   )
@@ -50,7 +50,7 @@ test_that("multiple functions, last, tag, properly formatted, no dontrun", {
 test_that("multiple functions, not last, tag, properly formatted, no dontrun", {
   expect_equal(
     identify_start_to_stop_of_roxygen_examples(testthat_file(
-      "identify-roxygen-examples/6-multiple-function-examples-not-last-proper-run.R"
+      "roxygen-examples-identify/6-multiple-function-examples-not-last-proper-run.R"
     )),
     list(seq(5, 5), seq(13, 17))
   )
