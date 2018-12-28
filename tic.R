57bad737ad81c136c3447784a0785a3c04d636cf
lorenzwalthert <lorenz.walthert@icloud.com>
2018-12-28 17:30:16 +0100

    fix CI with updated tic API

--- tic.R
+++ tic.R
@@ -1,4 +1,4 @@
-add_package_checks(notes_are_errors = getRversion() >= "3.2")
+add_package_checks(error_on = ifelse(getRversion() >= "3.2", "note", "error"))
 
 if (Sys.getenv("id_rsa") != "" && ci()$get_branch() == "master" && Sys.getenv("BUILD_PKGDOWN") != "") {
   # pkgdown documentation can be built optionally. Other example criteria:
