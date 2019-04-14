511f7a0d599f184707570bee4ac6cc0dfa962e18
lorenzwalthert <lorenz.walthert@icloud.com>
2019-04-15 00:14:33 +0200

    implement next_terminal(), used to check if next terminal is return statement

--- R/utils.R
+++ R/utils.R
@@ -121,6 +121,47 @@ previous_non_comment <- function(pd, pos) {
   last(setdiff(candidates, which(pd$token == "COMMENT")))
 }
 
+#' Tell me what the next terminal is
+#'
+#' If the first is a terminal, return it. If not, go inside it and search the
+#' next terminal
+#' @param pd A nest.
+#' @param stack Whether or not to also return information on the tokens that
+#'   are between `pd` and the first terminal, so the returned tibble can be
+#'   understood as a transition path from `pd` to the next terminal, instead of
+#'   the information at the terminal only. The order is inside-out,
+#'   i.e. the first non-terminal on top, the terminal last.
+#' @param vars The variables to return.
+#' @param tokens_exclude A vector with tokens to exclude. This can be helpful if
+#'   one wants to find the next token that is not a comment for example.
+#' @return
+#' Returns a tibble (which is **not** a valid parse table for
+#' `stack = TRUE`), with `vars` and another variable `position` that denotes
+#' the index each element in the transition. This can be helpful in conjunction
+#' with [purrr::pluck()] or [purrr::modify_in()] to reach the terminal in the
+#' nested structure.
+#' @keywords internal
+#' @examples
+#' pd <- compute_parse_data_nested("if (TRUE) f()")
+#' styler::next_terminal(pd)
+next_terminal <- function(pd,
+                          stack = FALSE,
+                          vars = c("pos_id", "token", "text"),
+                          tokens_exclude = c()) {
+
+  pd$position <- seq2(1, nrow(pd))
+  pd <- pd[!(pd$token %in% tokens_exclude),]
+  if (pd$terminal[1]) {
+    pd[1, c("position", vars)]
+  } else {
+    current <- next_terminal(pd$child[[1]], stack = stack, vars = vars, tokens_exclude = tokens_exclude)
+    if (stack) {
+      bind_rows(pd[1, c("position", vars)], current)
+    } else {
+      current
+    }
+  }
+}
 
 
 #' Find the index of the last comment in the sequence of comments-only tokens
