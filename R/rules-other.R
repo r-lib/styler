e1627db34190907f7eb4abf470772f7fbb14a5d0
lorenzwalthert <lorenz.walthert@icloud.com>
2019-04-15 00:19:21 +0200

    also add braces to for and while loops, independent of whether they contain a return() statement + refactor

--- R/rules-other.R
+++ R/rules-other.R
@@ -8,47 +8,68 @@ add_brackets_in_pipe_one <- function(pd, pos) {
   next_non_comment <- next_non_comment(pd, pos)
   rh_child <- pd$child[[next_non_comment]]
   if (nrow(rh_child) < 2 && rh_child$token == "SYMBOL") {
-    new_pos_ids <- create_pos_ids(pd$child[[next_non_comment]], 1, after = TRUE, n = 2)
+    child <- pd$child[[next_non_comment]]
+    new_pos_ids <- create_pos_ids(child, 1, after = TRUE, n = 2)
     new_pd <- create_tokens(
-      tokens = c("'('", "')'"), texts = c("(", ")"), pos_ids = new_pos_ids,
+      tokens = c("'('", "')'"),
+      texts = c("(", ")"),
+      pos_ids = new_pos_ids,
       lag_newlines = rep(0, 2)
     )
-    pd$child[[next_non_comment]] <-
-      bind_rows(pd$child[[next_non_comment]], new_pd) %>%
+    pd$child[[next_non_comment]] <- bind_rows(
+      pd$child[[next_non_comment]],
+      new_pd
+    ) %>%
       arrange(pos_id)
   }
   pd
 }
 
-#' Wrap if-else statement in curly braces
+#' Wrap if-else, while and for statements in curly braces
 #'
-#' Wrap an if-else statement in curly braces if it is not already wrapped in
-#' a such.
+#' Wrap statements in curly braces if it is not already wrapped in a such.
 #' @param pd A parse table.
 #' @param indent_by The amount of spaces used to indent an expression in curly
 #'   braces. Used for unindention.
 #' @keywords internal
-wrap_if_else_multi_line_in_curly <- function(pd, indent_by = 2) {
+#' @importFrom purrr when
+wrap_if_else_while_for_multi_line_in_curly <- function(pd, indent_by = 2) {
+  key_token <- when(pd,
+    is_cond_expr(.) ~ "')'",
+    is_while_expr(.) ~ "')'",
+    is_for_expr(.) ~ "forcond"
+  )
+  if (length(key_token) > 0) {
+    pd <- pd %>%
+      wrap_multiline_curly(indent_by,
+        space_after = ifelse(contains_else_expr(pd), 1, 0),
+        key_token = key_token
+      )
+    }
   if (is_cond_expr(pd)) {
     pd <- pd %>%
-      wrap_if_multiline_curly(indent_by,
-        space_after = ifelse(contains_else_expr(pd), 1, 0)
-      ) %>%
       wrap_else_multiline_curly(indent_by, space_after = 0)
   }
   pd
 }
 
-
-wrap_if_multiline_curly <- function(pd, indent_by, space_after = 1) {
-  if (if_part_requires_braces(pd)) {
-    closing_brace_ind <- which(pd$token == "')'")[1]
+#' Wrap a multi-line statement in curly braces
+#'
+#' @inheritParams wrap_if_else_while_for_multi_line_in_curly
+#' @inheritParams wrap_subexpr_in_curly
+#' @param key_token The token that comes right before the token that contains
+#'   the expression to be wrapped (ignoring comments). For if and while loops,
+#'   this is the closing "')'", for a for-loop it's "forcond".
+#' @keywords internal
+wrap_multiline_curly <- function(pd, indent_by, space_after = 1, key_token) {
+  to_be_wrapped_expr_with_child <- next_non_comment(
+    pd, which(pd$token == key_token)[1]
+  )
+  next_terminal <- next_terminal(pd[to_be_wrapped_expr_with_child,])$text
+  requires_braces <- if_for_while_part_requires_braces(pd, key_token)
+  if (requires_braces | next_terminal == "return") {
+    closing_brace_ind <- which(pd$token == key_token)[1]
     pd$spaces[closing_brace_ind] <- 1L
-
-    to_be_wrapped_expr_with_child <- next_non_comment(
-      pd,
-      which(pd$token == "')'")[1]
-    )
 
     all_to_be_wrapped_ind <- seq2(
       closing_brace_ind + 1L, to_be_wrapped_expr_with_child
@@ -63,6 +84,12 @@ wrap_if_multiline_curly <- function(pd, indent_by, space_after = 1) {
   pd
 }
 
+#' Add curly braces to else
+#'
+#' Wrap the else part of a conditional expression into curly braces if not
+#' already wraped into a such.
+#' @inheritParams wrap_multiline_curly
+#' @keywords internal
 wrap_else_multiline_curly <- function(pd, indent_by = 2, space_after = 0) {
   if (contains_else_expr(pd) &&
     pd_is_multi_line(pd) &&
@@ -81,7 +108,7 @@ wrap_else_multiline_curly <- function(pd, indent_by = 2, space_after = 0) {
 #' Wrap a sub-expression in curly braces
 #'
 #' Wraps some rows of a parse table into a sub-expression.
-#' @inheritParams wrap_if_else_multi_line_in_curly
+#' @inheritParams wrap_multiline_curly
 #' @param ind_to_be_wrapped The indices of the rows that should be wrapped
 #'   into a new expression.
 #' @inheritParams wrap_expr_in_curly
@@ -109,7 +136,14 @@ wrap_subexpr_in_curly <- function(pd,
     arrange(pos_id)
 }
 
-if_part_requires_braces <- function(pd) {
-  pd_is_multi_line(pd) &&
-    !is_curly_expr(pd$child[[next_non_comment(pd, which(pd$token == "')'")[1])]])
+#' Check if if, for or while loop expression require a braces.
+#'
+#' This is the case if they are multi-line and not yet wraped into curly
+#' braces.
+#' @inheritParams wrap_multiline_curly
+#' @keywords internal
+if_for_while_part_requires_braces <- function(pd, key_token) {
+  pos_first_key_token <- which(pd$token == key_token)[1]
+  child <- pd$child[[next_non_comment(pd, pos_first_key_token)]]
+  pd_is_multi_line(pd) && !is_curly_expr(child)
 }
