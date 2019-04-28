301a8619c4bb978cc89fc80f0dad0d0f284cf7ad
lorenzwalthert <lorenz.walthert@icloud.com>
2019-04-28 15:09:50 +0200

    let user specify transformers, not just style argument for flexibility. Rename option styler.addins.style -> styler.addins_style_transformer. Overhaul documentumentation for Addins, add life cycle.

--- R/addins.R
+++ R/addins.R
@@ -1,7 +1,24 @@
 #' Stylers for RStudio Addins
 #'
 #' Helper functions for styling via RStudio Addins.
-#'
+#' @section Addins:
+#' * Set style: Select the style transformers to use. For flexibility, the user
+#'   input is passed to the `transformers` argument, not the `style` argument, so
+#'   enter `styler::tidyverse_style(scope = "spaces")` in the Addin is
+#'   equivalent to `styler::style_text("1+1", scope = "spaces")` and
+#'   `styler::style_text("1+1", transformers = styler::tidyverse_style(scope = "spaces"))`
+#'   if the text to style is `1 + 1`. The style is memorized within an R
+#'   session. If you want it to persist over a session, set the option
+#'   `styler.addins_style_transformer` in your `.Rprofile`.
+#' * Style active file: Styles the active file, by default with
+#' [ tidyverse_style()] and `strict = TRUE`. You can set the style
+#'   with the Addin `Set style`.
+#' @section Life cycle:
+#' The way of specifying the style in the Addin is experimental. We currently
+#' think about letting the user specify the defaults for other style APIs like
+#' [styler::style_text()], either via R options, config files or other ways as
+#' well. See [r-lib/styler#319](https://github.com/r-lib/styler/issues/319) for
+#' the current status of this.
 #' @section Auto-Save Option:
 #' By default, both of the RStudio Addins will apply styling to the (selected)
 #' file contents without saving changes. Automatic saving can be enabled by
@@ -16,14 +33,13 @@
 #' @seealso [Sys.setenv()]
 NULL
 
-#' @describeIn styler_addins Styles the active file with [tidyverse_style()] and
-#'   `strict = TRUE`.
+
 #' @importFrom rlang abort
 #' @keywords internal
 style_active_file <- function() {
   communicate_addins_style()
   context <- get_rstudio_context()
-  transformer <- make_transformer(get_addins_style_fun()(),
+  transformer <- make_transformer(get_addins_style_fun(),
     include_roxygen_examples = TRUE, warn_empty = is_plain_r_file(context$path)
   )
 
@@ -45,6 +61,73 @@ style_active_file <- function() {
     rstudioapi::documentSave(context$id)
   }
   rstudioapi::setCursorPosition(context$selection[[1]]$range)
+}
+
+#' Styles the highlighted selection in a `.R` or `.Rmd` file.
+#' @importFrom rlang abort
+#' @keywords internal
+style_selection <- function() {
+  communicate_addins_style()
+  context <- get_rstudio_context()
+  text <- context$selection[[1]]$text
+  if (all(nchar(text) == 0)) abort("No code selected")
+  out <- style_text(text, style = get_addins_style_fun())
+  rstudioapi::modifyRange(
+    context$selection[[1]]$range, paste0(out, collapse = "\n"),
+    id = context$id
+  )
+  if (Sys.getenv("save_after_styling") == TRUE && context$path != "") {
+    rstudioapi::documentSave(context$id)
+  }
+}
+
+get_rstudio_context <- function() {
+  rstudioapi::getActiveDocumentContext()
+}
+
+#' @describeIn styler_addins asks the user to supply a style
+#' @importFrom rlang abort
+#' @keywords internal
+#' @importFrom rlang with_handlers abort
+set_style_transformers <- function() {
+  current_style <- get_addins_style_transformer_name()
+  new_style <-
+    rstudioapi::showPrompt(
+      "Select a style",
+      "Enter the name of a style transformer, e.g. `styler::tidyverse_style()`",
+      current_style
+    )
+  parsed_new_style <- with_handlers({
+    transformers <- eval(parse(text = new_style))
+    style_text(c("a = 2", "function() {", "NULL", "}"))
+    },
+    error = function(e) {
+      abort(paste0(
+        "The selected style transformers \"",
+        new_style, "\" is not valid: ", e$message
+      ))
+    }
+  )
+  options(styler.addins_style_transformer = new_style)
+  invisible(current_style)
+}
+
+#' Return the style function or name
+#'
+#' @keywords internal
+get_addins_style_transformer_name <- function() {
+  getOption("styler.addins_style_transformer", default = "styler::tidyverse_style()")
+}
+
+#' @rdname get_addins_style_transformer_name
+#' @keywords internal
+get_addins_style_fun <- function() {
+  eval(parse(text = get_addins_style_transformer_name()))
+}
+
+communicate_addins_style <- function() {
+  style_name <- get_addins_style_transformer_name()
+  cat("Using style transformers`", style_name, "`\n", sep = "")
 }
 
 #' Style a file as if it was an .R file
@@ -75,68 +158,4 @@ try_transform_as_r_file <- function(context, transformer) {
       }
     }
   )
-}
-
-#' @describeIn styler_addins Styles the highlighted selection in a `.R` or
-#'   `.Rmd` file.
-#' @importFrom rlang abort
-#' @keywords internal
-style_selection <- function() {
-  communicate_addins_style()
-  context <- get_rstudio_context()
-  text <- context$selection[[1]]$text
-  if (all(nchar(text) == 0)) abort("No code selected")
-  out <- style_text(text, style = get_addins_style_fun())
-  rstudioapi::modifyRange(
-    context$selection[[1]]$range, paste0(out, collapse = "\n"),
-    id = context$id
-  )
-  if (Sys.getenv("save_after_styling") == TRUE && context$path != "") {
-    rstudioapi::documentSave(context$id)
-  }
-}
-
-get_rstudio_context <- function() {
-  rstudioapi::getActiveDocumentContext()
-}
-
-#' Ask the user to supply a style
-#'
-#' @importFrom rlang abort
-#' @keywords internal
-#' @importFrom rlang with_handlers abort
-prompt_style <- function() {
-  current_style <- get_addins_style_name()
-  new_style <-
-    rstudioapi::showPrompt(
-      "Select a style",
-      "Enter the name of a style function, e.g. `styler::tidyverse_style`",
-      current_style
-    )
-  parsed_new_style <- with_handlers(
-    eval(parse(text = new_style)),
-    error = function(e) {
-      abort(paste0("The selected style \"", new_style, "\" is not valid: ", e$message))
-    }
-  )
-  options(styler.addins.style = new_style)
-  invisible(current_style)
-}
-
-#' Return the style function or name
-#'
-#' @keywords internal
-get_addins_style_name <- function() {
-  getOption("styler.addins.style", default = "styler::tidyverse_style")
-}
-
-#' @rdname get_addins_style_name
-#' @keywords internal
-get_addins_style_fun <- function() {
-  eval(parse(text = get_addins_style_name()))
-}
-
-communicate_addins_style <- function() {
-  style_name <- get_addins_style_name()
-  cat("Using style `", style_name, "`\n", sep = "")
 }
