3966a78fbc8df754c19b678dc298260d8a9efb7c
lorenzwalthert <lorenz.walthert@icloud.com>
2019-05-09 22:34:29 +0200

    upgrade testthat version

--- tests/testthat/test-escaping.R
+++ tests/testthat/test-escaping.R
@@ -2,5 +2,5 @@ test_that("escaping of characters works", {
   expect_warning(test_collection("escaping",
     transformer = style_text,
     write_back = TRUE
-  ), "different")
+  ), "macro '")
 })
