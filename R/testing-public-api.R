de2981719ab365df27deea50541cc993c1b4f1b8
lorenzwalthert <lorenz.walthert@icloud.com>
2019-06-19 23:11:37 +0200

    execute styling within temp dir because then the path to the file is just the file name and we don't need to bother about dir() returning C:\\xyz on windows and file.path() returning C:/ ect.

--- R/testing-public-api.R
+++ R/testing-public-api.R
@@ -6,12 +6,14 @@
 #' @return
 #' A character vector with the captured output of [style_file()] called on
 #' `file_in` ran in a temp dir to avoid side effects on the input file (because
-#' the next time the test would ran, the file would not need styling).
-#' In this output, the
-#' path has been removed so only the file name is contained in the return
-#' value to make the output portable across systems (in particular, to run it on
-#' CI systems). Since the horizontal rules width depend on the length of the
-#' path, the rules were also removed to standardize the ouptut of this function.
+#' the next time the test would ran, the file would not need styling). The
+#' styling is carried out with a temporary working directory change to keep
+#' filenames relative and avoid portability issues in the exact output
+#' comparison which is needed when the system that runs the unit testing (CI)
+#' is a different system than the one that created the reference value.
+#' This also implies that the ruler width, which depend on the path
+#' length, will again have the same width on all systems and is independent of
+#' how many characters the path of the temporary directory has.
 #' @importFrom utils capture.output
 #' @keywords internal
 catch_style_file_output <- function(file_in = c(
@@ -20,16 +22,15 @@ catch_style_file_output <- function(file_in = c(
                                       "dirty-sample-with-scope-tokens.R"
                                     ),
                                     encoding) {
-  temp_path <- copy_to_tempdir(do.call(testthat_file, as.list(file_in)))
-  removed_path <- gsub(normalizePath(dirname(temp_path)), "", capture.output(
-    style_file(temp_path, scope = "tokens")
-  ), fixed = TRUE)
-  removed_rules <- gsub(
-    ifelse(encoding == "utf8", "\u2500+", "-+"),
-    "", removed_path
-  )
+  file_in <- do.call(testthat_file, as.list(file_in))
+  temp_path <- copy_to_tempdir(file_in)
+  raw_output <- withr::with_dir(
+    dirname(temp_path),
+    capture.output(
+    style_file(basename(temp_path), scope = "tokens")
+  ))
   unlink(dirname(temp_path))
-  removed_rules
+  raw_output
 }
 
 ls_testable_encodings <- function() {
