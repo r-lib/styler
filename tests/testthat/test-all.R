f0507d3ee88ab0b4c7bfa08e59b9ec44e63dcdf7
lorenzwalthert <lorenz.walthert@icloud.com>
2019-07-29 15:47:25 +0200

    resolve dependencies.

--- tests/testthat/test-all.R
+++ tests/testthat/test-all.R
@@ -100,7 +100,7 @@ run_test("spell-check", suffix = "-language-success.md", cmd_args = "--lang=en_G
 ### . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ..
 ### depds-in-desc                                                           ####
 test_temp <- function(suffix, error_msg) {
-  path_desc <- test_path("reference-objects/DESCRIPTION")
+  path_desc <- testthat::test_path("reference-objects/DESCRIPTION")
   path_desc_temp <- fs::path(tempdir(), fs::path_file(path_desc))
   fs::file_copy(path_desc, tempdir(), overwrite = TRUE)
   new_desc <- desc::description$new(path_desc_temp)
