4545a3b6234000a73d96f6454cdfe4f55d631f11
lorenzwalthert <lorenz.walthert@icloud.com>
2019-07-29 15:20:29 +0200

    fix wordlist tests

--- tests/testthat/test-all.R
+++ tests/testthat/test-all.R
@@ -72,16 +72,16 @@ run_test("spell-check", suffix = "-fail.md", error_msg = "Spell check failed")
 
 # success with wordlist
 test_temp <- function() {
-  path_wordlist <- "inst/WORDLIST"
+  tempdir <- tempdir()
+  path_inst <- fs::path(tempdir, "inst")
+  path_wordlist <- fs::path(path_inst, "WORDLIST")
+  writeLines(c("fsssile", ""), path_wordlist)
   on.exit(fs::file_delete(path_wordlist))
-  fs::dir_create("inst")
-  fs::file_create(path_wordlist)
-  writeLines("fsssile", path_wordlist)
 
   run_test("spell-check",
     suffix = "-wordlist-success.md",
     error_msg = NULL,
-    copy = "inst/WORDLIST"
+    copy = path_wordlist
   )
 }
 test_temp()
@@ -92,3 +92,28 @@ run_test("spell-check", suffix = "-ignored-success.md", cmd_args = "--ignore_fil
 
 # uses lang argument
 run_test("spell-check", suffix = "-language-success.md", cmd_args = "--lang=en_GB")
+
+
+
+### . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ..
+### depds-in-desc                                                           ####
+test_temp <- function(suffix, error_msg) {
+  path_desc <- test_path("reference-objects/DESCRIPTION")
+  path_desc_temp <- fs::path(tempdir(), path_desc)
+  fs::file_copy(path_desc, tempdir(), overwrite = TRUE)
+  new_desc <- desc::description$new(path_desc_temp)
+  new_desc$set_dep("bliblablupp")
+  new_desc$write()
+  on.exit(fs::file_delete(path_desc_temp))
+  run_test("deps-in-desc",
+    suffix = suffix,
+    error_msg = error_msg,
+    copy = path_desc_temp
+  )
+}
+
+# succeed (call to library that is in description)
+test_temp(suffix = "-success.R", error_msg = NULL)
+
+# fail (call to library that is not in description)
+test_temp(suffix = "-fail.R", error_msg = "Dependency check failed")
