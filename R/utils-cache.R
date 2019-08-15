94c48ae181f349e2a90e2be63fc874efb3fd33ca
lorenzwalthert <lorenz.walthert@icloud.com>
2019-08-15 09:18:16 +0200

    cache administration tools

--- R/utils-cache.R
+++ R/utils-cache.R
@@ -14,6 +14,7 @@ hash_standardize <- function(x) {
 #' @param cache_subdir The subdir of the cache. Is equivalent to the
 #'   R.cache subdir when adding "styler" as a parent directory to
 #'   `cache_subdir`.
+#' @keywords internal
 cache_find_path <- function(cache_subdir = NULL) {
   if (is.null(cache_subdir)) {
     cache_subdir <- get_cache_subdir()
