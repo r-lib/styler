e199c641caff9c9991b3e2de54c49748acb7e270
lorenzwalthert <lorenz.walthert@icloud.com>
2019-08-15 09:14:00 +0200

    already styled files with a missing blank line at EOF should get one. Currently, because contents were identical, this was not the case.

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -6,3 +6,15 @@ test_that("non-comment-helpers", {
   expect_equal(previous_non_comment(child, 4), 2)
   expect_equal(next_non_comment(child, 2), 4)
 })
+
+test_that("files with and without blank EOF line are read correctly", {
+  expect_known_value(
+    read_utf8(test_path("reference-objects/missing-blank-at-EOF.R")),
+    test_path("reference-objects/return-read-utf8-missing-EOF")
+  )
+
+  expect_known_value(
+    read_utf8(test_path("reference-objects/non-missing-blank-at-EOF.R")),
+    test_path("reference-objects/return-read-utf8-non-missing-EOF")
+  )
+})
