b6c1092793c8b82a69d1255e2e55af330b180473
lorenzwalthert <lorenz.walthert@icloud.com>
2019-09-16 00:26:00 +0200

    add lintr

--- tic.R
+++ tic.R
@@ -1,5 +1,6 @@
 get_stage("install") %>%
-  add_code_step(remotes::install_deps(upgrade = FALSE))
+  add_code_step(remotes::install_deps(upgrade = FALSE)) %>%
+  k()
 
 get_stage("script") %>%
   add_code_step(source("tests/testthat/test-all.R"))
