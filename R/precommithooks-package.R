c3d916e42b6d559af6edc500f8c836bd3511c1e9
lorenzwalthert <lorenz.walthert@icloud.com>
2019-10-12 22:52:33 +0200

    import magrittr

---
+++ R/precommithooks-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+#' @importFrom magrittr %>%
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
