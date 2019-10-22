c4931c74670d7cd3a9d6bf67cc256ea71b055c94
lorenzwalthert <lorenz.walthert@icloud.com>
2019-10-22 20:53:23 +0200

    add tests for stylerignore

---
+++ tests/testthat/stylerignore/simple-in.R
@@ -0,0 +1,26 @@
+
+call(1 ) # styler: off
+# styler: off
+# also if there are more comments
+test_xkj("hier", na.rm = 3, py = 43
+         )
+
+
+x="new" # styler: off
+y=1 # none
+
+more_calls(with(arguments))
+# styler: on
+1 + 1
+a(!b)
+
+
+# --------
+x="new" # styler: off
+y=1 # none
+
+more_calls(with(arguments))
+# styler: off
+1 + 1
+a(!b)
+
