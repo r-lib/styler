aa5c90b5333df031946f6b56577bd50108938c90
lorenzwalthert <lorenz.walthert@icloud.com>
2019-10-29 00:24:19 +0100

    around 5% speed improvement by only running this rule when there are math tokens

--- R/rules-spacing.R
+++ R/rules-spacing.R
@@ -51,13 +51,19 @@ set_space_around_op <- function(pd_flat, strict) {
 style_space_around_math_token <- function(strict, zero, one, pd_flat) {
   # We remove spaces for zero (e.g., around ^ in the tidyverse style guide)
   # even for strict = FALSE to be consistent with the : operator
-  pd_flat %>%
-    style_space_around_token(
-      strict = TRUE, tokens = zero, level_before = 0L, level_after = 0L
-    ) %>%
-    style_space_around_token(
-      strict = strict, tokens = one, level_before = 1L, level_after = 1L
-    )
+  if (any(pd_flat$token %in% zero)) {
+    pd_flat <- pd_flat %>%
+      style_space_around_token(
+        strict = TRUE, tokens = zero, level_before = 0L, level_after = 0L
+      )
+  }
+  if (any(pd_flat$token %in% one)) {
+    pd_flat <-  pd_flat %>%
+      style_space_around_token(
+        strict = strict, tokens = one, level_before = 1L, level_after = 1L
+      )
+  }
+  pd_flat
 }
 
 #' Set spacing of token to a certain level
