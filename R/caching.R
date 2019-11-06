0d5eebae3d8b45c4de6b407a34dd8bcd69f0a5ca
lorenzwalthert <lorenz.walthert@icloud.com>
2019-11-06 22:27:53 +0100

    always overwrite the same key
    + does not grow cache over time
    + work directory specific.

--- R/caching.R
+++ R/caching.R
@@ -6,15 +6,3 @@ last_modified <- function(path_relative_cache, files = NULL) {
   all_files <- file.info(files)
   all_files[order(all_files$mtime, decreasing = TRUE)[1], ]
 }
-
-#' Check if a file is cached
-#' @param x An object, often the last modification time for hooks like 
-#'   `roxygenize`.
-#' @inheritParams last_modified
-#' @keywords internal
-is_cached <- function(x, path_relative_cache) {
-  !is.null(R.cache::findCache(
-    key = x,
-    path = path_relative_cache
-  ))
-}
