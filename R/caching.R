fbebbeee358e29a09f5fdcd58a9087f8ce082604
lorenzwalthert <lorenz.walthert@icloud.com>
2019-11-06 22:32:22 +0100

    avoid call to pre-commit package because .onLoad() takes too long

--- R/caching.R
+++
@@ -1,8 +0,0 @@
-#' Return info of last modified file
-#' @param path_relative_cache The path of the cache relative to the cache root.
-#' @param files Character vector with files to check.
-#' @keywords internal
-last_modified <- function(path_relative_cache, files = NULL) {
-  all_files <- file.info(files)
-  all_files[order(all_files$mtime, decreasing = TRUE)[1], ]
-}
