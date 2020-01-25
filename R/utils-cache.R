43f7676cb2933ae1793f761693f421bd1e65621d
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-01-25 22:19:49 +0100

    include comments

--- R/utils-cache.R
+++ R/utils-cache.R
@@ -124,7 +124,7 @@ cache_is_activated <- function(cache_name = NULL) {
 cache_by_expression <- function(text, transformers) {
   expressions <- parse(text = text, keep.source = TRUE) %>%
       utils::getParseData(includeText = TRUE)
-  expressions[expressions$parent == 0, "text"] %>%
+  expressions[expressions$parent < 1, "text"] %>%
     map(~cache_write(.x, transformers = transformers))
 }
 
