c49f1b9e62e1735a4e5e992ada7ac14e310217a4
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-02-15 02:14:37 +0100

    add test to make styler fail on problematic case
    fix test

--- tests/testthat/helpers-devel-options.R
+++ tests/testthat/helpers-devel-options.R
@@ -4,3 +4,7 @@ cache_deactivate()
 styler_version <- utils::packageDescription("styler", fields = "Version")
 clear_testthat_cache <- purrr::partial(cache_clear, "testthat", ask = FALSE)
 activate_testthat_cache <- purrr::partial(cache_activate, "testthat")
+fresh_testthat_cache <- function() {
+  clear_testthat_cache()
+  activate_testthat_cache()
+}
