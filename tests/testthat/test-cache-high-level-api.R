d98a7f3cf6c49c504ecc9af2a88bf574ab553042
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-02-22 10:43:35 +0100

    less strict speed assumptions

--- tests/testthat/test-cache-high-level-api.R
+++ tests/testthat/test-cache-high-level-api.R
@@ -29,7 +29,7 @@ test_that("activated cache brings speedup on style_text() API on character vecto
     text, text,
     fun = style_text
   )
-  expect_gt(n, 75)
+  expect_gt(n, 70)
 })
 
 test_that("activated cache brings speedup on style_text() API on character scalar", {
@@ -40,7 +40,7 @@ test_that("activated cache brings speedup on style_text() API on character scala
     text2, text2,
     fun = style_text
   )
-  expect_gt(n, 75)
+  expect_gt(n, 70)
 })
 
 
@@ -50,7 +50,7 @@ test_that("trailing line breaks are ignored for caching", {
   n <- n_times_faster_with_cache(text1, text2, clear = "all but last")
   expect_equal(cache_info()$n, 3)
   skip_on_cran()
-  expect_gt(n, 75)
+  expect_gt(n, 70)
 })
 
 test_that("trailing line breaks are ignored for caching in one scalar", {
@@ -59,7 +59,7 @@ test_that("trailing line breaks are ignored for caching in one scalar", {
   n <- n_times_faster_with_cache(text1, text2, clear = "all but last")
   expect_equal(cache_info()$n, 3)
   skip_on_cran()
-  expect_gt(n, 75)
+  expect_gt(n, 65)
 })
 
 test_that("trailing line breaks are ignored for caching in one scalar", {
@@ -71,7 +71,7 @@ test_that("trailing line breaks are ignored for caching in one scalar", {
   n <- n_times_faster_with_cache(text1, text2, clear = "all but last")
   expect_equal(cache_info()$n, 3)
   skip_on_cran()
-  expect_gt(n, 75)
+  expect_gt(n, 70)
 })
 
 capture.output(test_that("no speedup when tranformer changes", {
