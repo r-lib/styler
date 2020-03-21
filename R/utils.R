5b73b86401b79d8b5c12896e79c3433a0ad63d5b
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-03-21 14:29:45 +0100

    document in one place

--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,11 @@ is_package <- function(base_path = here::here()) {
 is_windows <- function() {
   identical(.Platform$OS.type, "windows")
 }
+
+
+extract_spell_check_regex <- function() {
+  yaml::read_yaml(here::here(".pre-commit-hooks.yaml")) %>%
+    purrr::keep(~ .x$id == "spell-check") %>%
+    magrittr::extract2(1) %>%
+    magrittr::extract2("exclude")
+}
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,10 @@
 library(testthat)
 library(precommit)
+versions <- versions::available.versions("desc")
+
+
+if (max(as.package_version(versions$desc$version)) > "1.2") {
+  rlang::abort("newer version of desc on CRAN. Adapt the warning and maybe add minimal version requirement?")
+}
 
 test_check("precommit")
