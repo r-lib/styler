8deae8adc45c46c8b644ea18dbc6b217b23965bc
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-04-02 19:30:54 +0200

    make tests more platform agnostic

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,20 +1,32 @@
 test_that("call capturing for pass", {
   expect_silent(captured <- call_and_capture("echo", 1))
-  expect_known_value(
-    captured,
-    test_path('reference-objects-captured-call-1')
+  expect_true(
+    captured$exit_status == 0
   )
-  expect_known_value(
+  expect_true(
+    length(captured$stdout) == 1
+  )
+
+  expect_true(
+    length(captured$stderr) == 0
+  )
+  expect_warning(
     communicate_captured_call(captured),
-    test_path('reference-objects-captured-call-2')
+    "stdout"
   )
 })
 
 test_that("call capturing for error", {
   expect_silent(captured <- call_and_capture("j23lkjsdi", 1))
-  expect_known_value(
-    captured,
-    test_path('reference-objects-captured-call-3')
+  expect_true(
+    captured$exit_status != 0
+  )
+  expect_true(
+    length(captured$stdout) == 0
+  )
+
+  expect_true(
+    length(captured$stderr) == 1
   )
   expect_error(
     communicate_captured_call(captured),
