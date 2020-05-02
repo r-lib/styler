57c3e3e78b911742493f557b9c090836e0173890
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-05-02 23:58:29 +0200

    more R CMD check fixes

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,10 @@
 library(testthat)
 library(precommit)
-versions <- versions::available.versions("desc")
-
-
-if (max(as.package_version(versions$desc$version)) > "1.2") {
-  rlang::abort("newer version of desc on CRAN. Adapt the warning and maybe add minimal version requirement?")
+if (on_cran()) {
+  versions <- versions::available.versions("desc")
+  if (max(as.package_version(versions$desc$version)) > "1.2") {
+    rlang::abort("newer version of desc on CRAN. Adapt the warning and maybe add minimal version requirement?")
+  }
 }
 
 test_check("precommit")
