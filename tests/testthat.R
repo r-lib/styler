6d4b33e32bcc2ed3e9ade8b9eee68b57c1705b7c
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-05-03 00:14:50 +0200

    move to test where package is loaded and we can use devtools infra within test_that()

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,3 @@
 library(testthat)
 library(precommit)
-if (on_cran()) {
-  versions <- versions::available.versions("desc")
-  if (max(as.package_version(versions$desc$version)) > "1.2") {
-    rlang::abort("newer version of desc on CRAN. Adapt the warning and maybe add minimal version requirement?")
-  }
-}
-
 test_check("precommit")
---
+++ tests/testthat/test-assumptions.R
@@ -0,0 +1,7 @@
+test_that("documentation regarding desc is still accurate", {
+  skip_on_cran()
+    versions <- versions::available.versions("desc")
+    if (max(as.package_version(versions$desc$version)) > "1.2") {
+      rlang::abort("newer version of desc on CRAN. Adapt the warning and maybe add minimal version requirement?")
+    }
+})
