ef26f402ea8fe18c42de4366b7cdc5bcba103791
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-05-05 14:05:01 +0200

    simplify tic, only build pkgdown on one machine

--- tic.R
+++ tic.R
@@ -2,16 +2,7 @@ Sys.setenv(LINTR_COMMENT_BOT = "false")
 do_package_checks(
   error_on = ifelse(tic::ci_has_env("EXTERNAL_INSTALLATION"), "warning", "note")
 )
-if (Sys.getenv("id_rsa") != "" && ci()$get_branch() == "master") {
-  # pkgdown documentation can be built optionally. Other example criteria:
-  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
-  # - `ci()$is_tag()`: Only for tags, not for branches
-  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
-  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
 
-  get_stage("deploy") %>%
-    add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy("docs", "gh-pages"))
+if (ci_has_env("BUILD_PKGDOWN")) {
+  do_pkgdown()
 }
