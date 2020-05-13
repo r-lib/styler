589a33b8f0f5e78fea98b0965a29baa378f9192a
Michael Quinn <msquinn@google.com>
2020-05-13 14:14:53 -0400

    Don't print()

--- tests/testthat/test-public_api.R
+++ tests/testthat/test-public_api.R
@@ -130,18 +130,21 @@ test_that("styler can style Rmd file", {
   }))
 
   capture_output(expect_warning(
-    styled <- style_file(testthat_file("public-api", "xyzfile_rmd", "random2.Rmd"), strict = FALSE)
+    styled <- style_file(testthat_file("public-api", "xyzfile_rmd",
+        "random2.Rmd"), strict = FALSE)
   ))
   expect_false(styled$changed)
 })
 
 test_that("styler handles malformed Rmd file and invalid R code in chunk", {
   capture_output(expect_warning(
-    style_file(testthat_file("public-api", "xyzfile_rmd", "random3.Rmd"), strict = FALSE)
+    style_file(testthat_file("public-api", "xyzfile_rmd", "random3.Rmd"), strict
+      = FALSE)
   ))
 
   capture_output(expect_warning(
-    style_file(testthat_file("public-api", "xyzfile_rmd", "random4.Rmd"), strict = FALSE)
+    style_file(testthat_file("public-api", "xyzfile_rmd", "random4.Rmd"), strict
+      = FALSE)
   ))
 })
 
@@ -205,7 +208,6 @@ test_that("Messages can be suppressed", {
         output <- catch_style_file_output(c(
           "public-api", "xyzdir-dirty", "dirty-sample-with-scope-spaces.R"
         ), encoding = encoding)
-        print(output)
         expect_equal(output, character(0))
       }
     )
@@ -288,18 +290,21 @@ test_that("styler can style Rnw file", {
   }))
 
   capture_output(expect_warning(
-    styled <- style_file(testthat_file("public-api", "xyzfile-rnw", "random2.Rnw"), strict = FALSE)
+    styled <- style_file(testthat_file("public-api", "xyzfile-rnw",
+        "random2.Rnw"), strict = FALSE)
   ))
   expect_false(styled$changed)
 })
 
 test_that("styler handles malformed Rnw file and invalid R code in chunk", {
   capture_output(expect_warning(
-    style_file(testthat_file("public-api", "xyzfile-rnw", "random3.Rnw"), strict = FALSE)
+    style_file(testthat_file("public-api", "xyzfile-rnw", "random3.Rnw"), strict
+      = FALSE)
   ))
 
   capture_output(expect_warning(
-    style_file(testthat_file("public-api", "xyzfile-rnw", "random4.Rnw"), strict = FALSE)
+    style_file(testthat_file("public-api", "xyzfile-rnw", "random4.Rnw"), strict
+      = FALSE)
   ))
 })
 
