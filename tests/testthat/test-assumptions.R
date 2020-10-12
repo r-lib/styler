08c6a7eb02488c60cb66f927392a6fe91e348ab0
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-10-12 15:35:47 +0200

    only check if reticulate is installed

--- tests/testthat/test-assumptions.R
+++ tests/testthat/test-assumptions.R
@@ -7,6 +7,7 @@ test_that("documentation regarding desc is still accurate", {
 
 test_that("reticulate dev version still required to make tests pass on conda macOS because of https://github.com/rstudio/reticulate/issues/820", {
   skip_on_cran()
+  skip_if_not_installed('reticulate')
   if (packageVersion('reticulate') >= 1.18) {
     rlang::abort("You can depend on the latest CRAN release of reticulate instead of the dev version and remove this assumption test.")
   }
