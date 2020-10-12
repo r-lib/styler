ff0f55e83477d81aee0a8346ca34b678118e2433
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-10-12 15:21:22 +0200

    assert version requirements

--- tests/testthat/test-assumptions.R
+++ tests/testthat/test-assumptions.R
@@ -1,7 +1,13 @@
 test_that("documentation regarding desc is still accurate", {
   skip_on_cran()
-  versions <- versions::available.versions("desc")
-  if (max(as.package_version(versions$desc$version)) > "1.2") {
+  if (packageVersion('desc') >= 1.3) {
     rlang::abort("newer version of desc on CRAN. Adapt the warning and maybe add minimal version requirement?")
+  }
+})
+
+test_that("reticulate dev version still required to make tests pass on conda macOS because of https://github.com/rstudio/reticulate/issues/820", {
+  skip_on_cran()
+  if (packageVersion('reticulate') >= 1.18) {
+    rlang::abort("You can depend on the latest CRAN release of reticulate instead of the dev version and remove this assumption test.")
   }
 })
