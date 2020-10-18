98ca3686290b7f729e355d1528e7c70e91b0b14a
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-10-18 19:52:03 +0200

    fix plot

--- bench/01-declarations.R
+++ bench/01-declarations.R
@@ -7,25 +7,30 @@ plot_against_base <- function(new_bm,
                               new_bm_label = deparse(substitute(new_bm)),
                               file = paste0("plots/", new_bm_label, ".pdf")) {
   new_bm <- bench::as_bench_mark(new_bm)
+  name <- unique(new_bm$name)
+  stopifnot(length(name) == 1)
   branches <- gert::git_branch_list()
   last_commit_base_branch <- branches[branches$name == Sys.getenv("GITHUB_BASE_REF"), "commit", drop = TRUE]
   bm <- bench::cb_read()
   commit_is_reference <- bm$commit_hash == last_commit_base_branch
   if (any(commit_is_reference) && Sys.getenv("GITHUB_BASE_REF") != "") {
     # if a pull request
-    reference <- bm[commit_is_reference, "benchmarks"][[1]][[1]]
+    reference <- bm[commit_is_reference, "benchmarks"][[1]][[1]] %>%
+      dplyr::filter(.data$name %in% !!name)
     reference$expression <- bench:::new_bench_expr(Sys.getenv("GITHUB_BASE_REF"))
     new_bm$expression <- bench:::new_bench_expr(Sys.getenv("GITHUB_HEAD_REF"))
-    new_bm <- rbind(reference, new_bm)
+    new_bm <- dplyr::bind_rows(reference, new_bm)
+    new_bm$branch <- factor(new_bm$expression)
   }
   plot <- ggplot2::ggplot(new_bm) +
     ggplot2::geom_boxplot(ggplot2::aes(
-      x = name, ymin = p0,
+      x = branch, ymin = p0,
       ymax = p100, lower = p25,
       middle = p50, upper = p75
     ),
     stat = "identity"
-    )
+    ) +
+    ggplot2::ggtitle(name)
 
   ggplot2::ggsave(file, plot)
 }
