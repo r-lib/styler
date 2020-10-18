b663a4049fa8cda9ed887712e5051e95f5c29d75
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-10-18 21:25:05 +0200

    better handling of bm

--- bench/01-declarations.R
+++ bench/01-declarations.R
@@ -7,23 +7,24 @@ plot_against_base <- function(new_bm,
                               new_bm_label = deparse(substitute(new_bm)),
                               file = paste0("plots/", new_bm_label, ".pdf")) {
   new_bm <- bench::as_bench_mark(new_bm)
+  new_bm$expression <- bench:::new_bench_expr(Sys.getenv("GITHUB_HEAD_REF"))
   name <- unique(new_bm$name)
   stopifnot(length(name) == 1)
   branches <- gert::git_branch_list()
   last_commit_base_branch <- branches[branches$name == Sys.getenv("GITHUB_BASE_REF"), "commit", drop = TRUE]
   bm <- bench::cb_read()
-  print("bm is")
-  print(bm)
   commit_is_reference <- bm$commit_hash == last_commit_base_branch
-  if (any(commit_is_reference) && Sys.getenv("GITHUB_BASE_REF") != "" && !is.null(bm) && nrow(bm) > 0) {
+  if (any(commit_is_reference) && Sys.getenv("GITHUB_BASE_REF") != "") {
     # if a pull request
-    reference <- bm[commit_is_reference, "benchmarks"][[1]][[1]] %>%
-      dplyr::filter(.data$name %in% !!name)
-    reference$expression <- bench:::new_bench_expr(Sys.getenv("GITHUB_BASE_REF"))
-    new_bm$expression <- bench:::new_bench_expr(Sys.getenv("GITHUB_HEAD_REF"))
-    new_bm <- dplyr::bind_rows(reference, new_bm)
-    new_bm$branch <- factor(new_bm$expression)
+    reference <- bm[commit_is_reference, "benchmarks"][[1]][[1]]
+    if (nrow(reference) > 0 && "name" %in% names(reference)) {
+      reference <- reference %>%
+        dplyr::filter(.data$name %in% !!name)
+      reference$expression <- bench:::new_bench_expr(Sys.getenv("GITHUB_BASE_REF"))
+      new_bm <- dplyr::bind_rows(reference, new_bm)
+    }
   }
+  new_bm$branch <- factor(new_bm$expression)
   plot <- ggplot2::ggplot(new_bm) +
     ggplot2::geom_boxplot(ggplot2::aes(
       x = branch, ymin = p0,
