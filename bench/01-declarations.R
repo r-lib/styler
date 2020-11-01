94c9d376fb830ba2aeb2c250df7d50f793d708c5
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-11-01 17:15:36 +0100

    add an additional check for name

--- bench/01-declarations.R
+++ bench/01-declarations.R
@@ -18,16 +18,18 @@ plot_against_base <- function(new_bm,
     # if a pull request
     reference <- bm[commit_is_reference, "benchmarks"][[1]][[1]]
     # if benchmark exists in base branch
-    reference <- reference %>%
-      dplyr::filter(.data$name %in% !!name)
-    if (nrow(reference) > 0) {
-      # if benchmark exists in base branch
-      reference$expression <- bench:::new_bench_expr(Sys.getenv("GITHUB_BASE_REF"))
-      new_bm <- dplyr::bind_rows(reference, new_bm)
-      stopifnot(nrow(new_bm) == 2)
-      diff_in_percent <- round(100 * diff(new_bm$p50) / new_bm$p50[1])
-      pr_comment <- glue::glue("* {name}: {new_bm$p50[1]} -> {new_bm$p50[2]} ({diff_in_percent}%)\n")
-      cat(pr_comment, file = "pr-comment/info.txt", append = TRUE)
+    if ("name" %in% names(reference)) {
+      reference <- reference %>%
+        dplyr::filter(.data$name %in% !!name)
+      if (nrow(reference) > 0) {
+        # if benchmark exists in base branch
+        reference$expression <- bench:::new_bench_expr(Sys.getenv("GITHUB_BASE_REF"))
+        new_bm <- dplyr::bind_rows(reference, new_bm)
+        stopifnot(nrow(new_bm) == 2)
+        diff_in_percent <- round(100 * diff(new_bm$p50) / new_bm$p50[1])
+        pr_comment <- glue::glue("* {name}: {new_bm$p50[1]} -> {new_bm$p50[2]} ({diff_in_percent}%)\n")
+        cat(pr_comment, file = "pr-comment/info.txt", append = TRUE)
+      }
     }
   }
   new_bm$branch <- factor(new_bm$expression)
