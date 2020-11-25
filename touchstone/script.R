1f59b3ba322375031c293bd6e1eb19f94b37dcf5
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-11-26 00:05:44 +0100

    loop

--- touchstone/script.R
+++ touchstone/script.R
@@ -1,27 +1,36 @@
 # touchstone:::touchstone_clear() # deletes itself and sources
-refs <- c(Sys.getenv("GITHUB_BASE_REF"), Sys.getenv("GITHUB_HEAD_REF"))
-warning(refs)
-timings <- touchstone::benchmark_run_ref(
-  refs,
-  expr_before_benchmark = c("library(styler)", "cache_deactivate()"),
-  expr_to_benchmark = 'style_pkg("touchstone/sources/here", filetype = c("R", "rmd"))',
-  n = 10,
+refs <- c(Sys.getenv("GITHUB_BASE_REF", "touchstone"), Sys.getenv("GITHUB_HEAD_REF", "touchstone"))
+exprs_to_benchmark <- list(
+  cache_applying = 'style_pkg("touchstone/sources/here", filetype = c("R", "rmd"))'
 )
+for (benchmark in names(exprs_to_benchmark)) {
+  timings <- touchstone::benchmark_run_ref(
+    refs,
+    expr_before_benchmark = c("library(styler)", "cache_deactivate()"),
+    !!!exprs_to_benchmark[[benchmark]],
+    n = 10
+  )
 
-timings <- touchstone::benchmark_read(refs)
+  timings <- touchstone::benchmark_read(benchmark, refs[1])
 
-library(ggplot2)
-library(magrittr)
-timings %>%
-  ggplot(aes(x = elapsed, color = ref)) +
-  geom_density()
+  library(ggplot2)
+  library(magrittr)
+  timings %>%
+    ggplot(aes(x = elapsed, color = ref)) +
+    geom_density()
+  fs::path("touchstone/plots/", benchmark) %>%
+    fs::path_ext_set("png") %>%
+    ggsave()
 
-ggsave("touchstone/plots/density.png")
+  tbl <- timings %>%
+    dplyr::group_by(.data$ref) %>%
+    dplyr::summarise(m = mean(.data$elapsed)) %>%
+    tibble::deframe()
 
-tbl <- timings %>%
-  dplyr::group_by(.data$ref) %>%
-  dplyr::summarise(m = mean(.data$elapsed)) %>%
-  tibble::deframe()
-
-diff_percent <- round(100 * (tbl[refs[2]] - tbl[refs[1]]) / tbl[refs[1]], 1)
-writeLines(glue::glue("{round(tbl[refs[1]], 2)} -> {round(tbl[refs[2]], 2)} ({diff_percent}%)"), "touchstone/pr-comment/info.txt")
+  diff_percent <- round(100 * (tbl[refs[2]] - tbl[refs[1]]) / tbl[refs[1]], 1)
+  cat(
+    glue::glue("{benchmark}: round(tbl[refs[1]], 2)} -> {round(tbl[refs[2]], 2)} ({diff_percent}%)"),
+    file = "touchstone/pr-comment/info.txt",
+    append = TRUE
+  )
+}
