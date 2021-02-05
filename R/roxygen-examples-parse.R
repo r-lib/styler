a14be5e845ed7f42c5bb804c9cbdfab0bf93e10f
Lorenz <lorenz.walthert@icloud.com>
2021-02-05 17:07:10 +0100

    fundamentally re-work how roxygen is parsed with roxygen infra to get escaping right

--- R/roxygen-examples-parse.R
+++ R/roxygen-examples-parse.R
@@ -15,11 +15,39 @@
 #' ))
 #' @keywords internal
 parse_roxygen <- function(roxygen) {
-  connection <- remove_roxygen_mask(roxygen) %>%
+  roxygen <- c(
+    "#' Example", "#' @examples",
+    gsub("^#'\\s*@examples\\s*(.*)", "#' \\1", roxygen, perl = TRUE),
+    "x <- 1"
+  )
+
+  connection <- roxygen2::roc_proc_text(
+    roxygen2::rd_roclet(),
+    paste(roxygen, collapse = "\n")
+  )[[1]]$get_section("examples") %>%
+    as.character() %>%
+    .[-1] %>%
     textConnection()
-  parsed <- connection %>%
-    tools::parse_Rd(fragment = TRUE) %>%
-    as.character(deparse = TRUE)
+  suppressWarnings(
+    parsed <- tools::parse_Rd(connection, fragment = TRUE) %>%
+      as.character(deparse = FALSE)
+  )
+  parsed <- rlang::with_handlers(
+    {
+      # dont match word boundary, check this matches all keywords
+      parse_safely(paste0(gsub("^\\\\[[:alpha:]]*\\b", "", parsed, perl = TRUE), collapse = ""))
+      parsed
+    },
+    error = function(e) {
+      parsed_ <- gsub("\\\\.*\\w", "", parsed)
+      if (any(parsed == "}")) {
+        parsed[-last(which(parsed == "}"))]
+      } else {
+        parse_safely(gsub("\\\\.*\\w", "", parsed)) # this will error informatively
+      }
+    }
+  )
+  parsed <- parsed[parsed != ""]
   is_line_break <- parsed[1] == "\n"
   close(connection)
   c(parsed[1][!is_line_break], parsed[-1])
--- tests/testthat/test-escaping.R
+++ tests/testthat/test-escaping.R
@@ -1,5 +1,9 @@
 test_that("escaping of characters works", {
-  expect_warning(test_collection("escaping",
+  expect_warning(test_collection("escaping", "basic",
     transformer = style_text
-  ), "macro '")
+  ), NA)
+
+  expect_error(test_collection("escaping", "fail",
+    transformer = style_text
+  ), "<text>:1:7: unexpected ")
 })
