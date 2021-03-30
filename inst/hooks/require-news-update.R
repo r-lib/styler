cac5ec5e18c6989873b4ebdf07b7c0d101384399
Lorenz <lorenz.walthert@icloud.com>
2021-03-30 10:40:18 +0200

    check with upstream diff

--- inst/hooks/require-news-update.R
+++ inst/hooks/require-news-update.R
@@ -1,5 +1,10 @@
 #! /usr/local/bin/Rscript
-args <- commandArgs(trailingOnly = TRUE)
+args <- system2(
+  "git",
+  c("diff", "upstream/master", "--name-only"),
+  stdout = TRUE
+)
+
 if (!any(args == "NEWS.md")) {
   rlang::abort("Must have a news entry before pushing.")
 }
