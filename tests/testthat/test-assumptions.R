5cf5ac153de92bd720c4a789f5c3e10876623643
Lorenz Walthert <lorenz.walthert@icloud.com>
2021-04-09 22:13:40 +0200

    drop assumption test, hope people use latest version of desc

--- tests/testthat/test-assumptions.R
+++
@@ -1,6 +0,0 @@
-test_that("documentation regarding desc is still accurate", {
-  skip_on_cran()
-  if (packageVersion("desc") >= 1.3 && Sys.Date() > as.Date("2021-04-01")) {
-    rlang::abort("newer version of desc on CRAN. Adapt the warning and maybe add minimal version requirement?")
-  }
-})
