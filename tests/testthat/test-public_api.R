60fc43a7f31a7a465649316f9019b51f24a99f21
Lorenz <lorenz.walthert@icloud.com>
2021-05-06 09:28:15 +0200

    re-organize tests: only error tets are in public-api, the others are moved to rmd. Explicit warning message matching, avoid empty chunks.

--- tests/testthat/test-public_api.R
+++ tests/testthat/test-public_api.R
@@ -136,22 +136,14 @@ test_that("styler can style Rmd file", {
 })
 
 test_that("styler handles malformed Rmd file and invalid R code in chunk", {
-  capture.output(expect_warning(
-    style_file(testthat_file("public-api", "xyzfile_rmd", "random3.Rmd"), strict = FALSE),
-    NA
+  capture_output(expect_warning(
+    style_file(testthat_file("public-api", "xyzfile_rmd", "random4.Rmd"), strict = FALSE),
+    "3: "
   ))
 
   capture_output(expect_warning(
-    style_file(testthat_file("public-api", "xyzfile_rmd", "random4.Rmd"), strict = FALSE)
-  ))
-
-  capture_output(expect_warning(
-    style_file(testthat_file("public-api", "xyzfile_rmd", "random5.Rmd"), strict = FALSE)
-  ))
-
-  capture_output(expect_warning(
-    style_file(testthat_file("public-api", "xyzfile_rmd", "random6.Rmd"), strict = FALSE),
-    NA
+    style_file(testthat_file("public-api", "xyzfile_rmd", "random7.Rmd"), strict = FALSE),
+    "Malformed file"
   ))
 })
 
