8c1c63b5133a2b1fe23a4135a1fdf2a9754f3425
Lorenz <lorenz.walthert@icloud.com>
2021-05-12 11:00:28 +0200

    temporarily lower n

--- touchstone/script.R
+++ touchstone/script.R
@@ -5,7 +5,7 @@ refs_install()
 benchmark_run_ref(
   expr_before_benchmark = c("library(styler)", "cache_deactivate()"),
   without_cache = 'style_pkg("touchstone/sources/here", filetype = c("R", "rmd"))',
-  n = 30
+  n = 3
 )
 
 styler::cache_clear()
@@ -13,7 +13,7 @@ styler::cache_clear()
 benchmark_run_ref(
   expr_before_benchmark = c("library(styler)", "cache_activate()"),
   cache_applying = 'style_pkg("touchstone/sources/here", filetype = c("R", "rmd"))',
-  n = 30
+  n = 3
 )
 
 styler::cache_clear()
