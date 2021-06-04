e8698e5522b60291445d51c8224faa38440c5406
Lorenz Walthert <lorenz.walthert@icloud.com>
2021-06-04 08:59:03 +0200

    clean up Rbuildignore

--- tic.R
+++
@@ -1,8 +0,0 @@
-Sys.setenv(LINTR_COMMENT_BOT = "false")
-do_package_checks(
-  error_on = ifelse(tic::ci_has_env("EXTERNAL_INSTALLATION"), "warning", "note")
-)
-
-if (ci_has_env("BUILD_PKGDOWN")) {
-  do_pkgdown()
-}
