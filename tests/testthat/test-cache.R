030a8e48052fb69f6d487c565af76b9f5e7bd243
Patrick Schratz <patrick.schratz@gmail.com>
2021-07-02 14:59:00 +0200

    Update test-cache.R

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -40,7 +40,7 @@ test_that("CLI API works for style-files", {
     "style-files",
     suffix = "-cache-success.R",
     env = paste0("R_CACHE_ROOTPATH=", R.cache_root),
-    msg = "Using persistant"
+    msg = "Using persistent"
   )
 })
 
@@ -66,7 +66,7 @@ test_that("CLI API works for roxygenize", {
     "roxygenize",
     suffix = "-cache-success.R",
     env = paste0("R_CACHE_ROOTPATH=", R.cache_root),
-    msg = "Using persistant",
+    msg = "Using persistent",
     file_transformer = function(x) {
       git2r::init()
       x
