da5fb6a4c6e6ba2da5b0169d4acca8c38ec5125e
Lorenz <lorenz.walthert@icloud.com>
2021-09-11 20:38:05 +0200

    test we did not use cache with tests

--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -1,4 +1,11 @@
 test_that("styler tests did not use R.cache in user root", {
+  skip_on_cran()
+  expect_true(
+    length(list.files(R.cache::getCachePath("styler"), recursive = TRUE)) == 0
+  )
+})
+
+test_that("clear Cache", {
   R.cache::clearCache(R.cache::getCachePath("styler"))
   skip_on_cran()
   expect_true(
