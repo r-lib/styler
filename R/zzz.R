ba35736d30847955ed98195126b4801cc8ed1778
Lorenz <lorenz.walthert@icloud.com>
2021-09-22 10:35:36 +0200

    emergency release to comply with CRAN policy

--- R/zzz.R
+++ R/zzz.R
@@ -13,8 +13,20 @@
   )
   toset <- !(names(op.styler) %in% names(op))
   if (any(toset)) options(op.styler[toset])
-  remind_removing_old_cache()
+  remove_cache_old_versions()
+  remove_old_cache_files()
   invisible()
+}
+
+remove_old_cache_files <- function() {
+  all_cached <- list.files(
+    R.cache::getCachePath(c("styler", styler_version)),
+    full.names = TRUE
+  )
+  date_boundary <- Sys.time() - 60 * 60 * 24 * 6
+  file.remove(
+    all_cached[file.info(all_cached)$mtime < date_boundary]
+  )
 }
 
 #' Ask people to remove the cache
@@ -23,39 +35,17 @@
 #' reasons (https://stackoverflow.com/questions/55772436/readline-does-not-prompt-user-input-from-rprofile-site-in-rstudio)
 #' So we better don't use the prompt and issue a message only.
 #' @keywords internal
-remind_removing_old_cache <- function() {
-  if (interactive() && getOption("styler.interactive_ask_remove_old_caches", TRUE)) {
-    dirs <- list.dirs(R.cache::getCachePath("styler"), recursive = FALSE)
-    if (length(dirs) < 1) {
-      return()
-    }
-    dirs <- dirs[unname(sapply(dirs, function(x) length(list.files(x)) > 0))]
-    package_versions <- package_version(basename(dirs), strict = FALSE)
-    package_versions <- package_versions[!is.na(package_versions)]
-    old_package_versions <- package_versions[package_versions < styler_version]
-    if (length(old_package_versions) < 1) {
-      return()
-    }
-
-    cmd <- glue::glue("styler::cache_clear(\"{basename(dirs)}\", ask = FALSE)") %>%
-      paste0(collapse = "\n")
-    cli::cli_alert_info(paste0(
-      "You are using {{styler}} version {styler_version} but we found ",
-      "caches for older versions of {{styler}}.\n",
-      "You can delete them with the following commands:"
-    ))
-    cat("\n")
-    cli::cli_code(cmd)
-    cat("\n")
-    cli::cli_alert_info(
-      paste(
-        "We'll remind you every time you update {{styler}}.",
-        "To suppress this prompt in the future:"
-      )
-    )
-    cat("\n")
-    cli::cli_code(
-      'options("styler.interactive_ask_remove_old_caches" = FALSE)'
-    )
+remove_cache_old_versions <- function() {
+  dirs <- list.dirs(R.cache::getCachePath("styler"), recursive = FALSE)
+  if (length(dirs) < 1) {
+    return()
   }
+  old_package_dirs <- dirs[basename(dirs) != as.character(styler_version)]
+  if (length(old_package_dirs) < 1) {
+    return()
+  }
+  purrr::walk(old_package_dirs, function(dir) {
+    cache_clear(basename(dir), ask = FALSE)
+    file.remove(dir)
+  })
 }
