412ca7dd3ba8b74354817096e37f9ed7cd5a59b6
Lorenz Walthert <lorenz.walthert@icloud.com>
2021-10-27 11:31:21 +0200

    I think we want this

--- tests/testthat/test-utf8.R
+++ tests/testthat/test-utf8.R
@@ -7,9 +7,9 @@ context("UTF-6 with 4.2")
 test_that("styling unicode characters on Windows", {
   skip_if_not(.Platform$OS.type[[1]] == "windows")
   skip_if(getRversion() >= '4.2')
-
+  skip_on_cran()
   expect_identical(
     styler::style_text('suit <- "♠"'),
-    structure("suit <- \"<U+2660>\"", class = "vertical")
+    structure("suit <- \"♠\"", class = "vertical")
   )
 })
