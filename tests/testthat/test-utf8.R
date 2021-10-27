8b76c8c121c3b2673ea19a072572e5bd4403a595
Lorenz Walthert <lorenz.walthert@icloud.com>
2021-10-27 11:32:26 +0200

    ugh

--- tests/testthat/test-utf8.R
+++ tests/testthat/test-utf8.R
@@ -1,7 +1,5 @@
 context("UTF-6 with 4.2")
 
-# TODO: change this test once R 4.2 is released
-
 # for more, see: https://github.com/r-lib/styler/issues/847
 # this behavior is likely to change with R 4.2
 test_that("styling unicode characters on Windows", {
