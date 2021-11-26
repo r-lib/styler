5e1941f6ead2f9584ebf9d674d36c19ec384d9d3
Lorenz Walthert <lorenz.walthert@icloud.com>
2021-11-26 18:07:40 +0100

    decouple .pre-commit-config.yaml template from the one used in this repo

--- inst/hooks/local/spell-check-exclude-to-config.R
+++ inst/hooks/local/spell-check-exclude-to-config.R
@@ -1,10 +1,5 @@
 #!/usr/bin/env Rscript
 library(magrittr)
-exclude_hooks <- yaml::read_yaml(here::here(".pre-commit-hooks.yaml")) %>%
-  purrr::keep(~ .x$id == "spell-check") %>%
-  magrittr::extract2(1) %>%
-  purrr::pluck("exclude")
-
 read_config <- function(path) {
   yaml::read_yaml(path) %>%
     purrr::pluck("repos") %>%
@@ -13,17 +8,10 @@ read_config <- function(path) {
     purrr::keep(~ .x$id == "spell-check") %>%
     purrr::pluck(1, "exclude")
 }
-
-if (!identical(exclude_hooks, read_config(here::here("inst/pre-commit-config-pkg.yaml")))) {
+hooks_pkg <- read_config(here::here("inst/pre-commit-config-pkg.yaml"))
+hooks_proj <- read_config(here::here("inst/pre-commit-config-proj.yaml"))
+if (!identical(hooks_proj, hooks_pkg)) {
   rlang::abort(paste0(
-    "The `exclude` key from the spell-check hook should be copied from `.pre-commit-hooks.yaml` to ",
-    "`inst/pre-commit-config-pkg.yaml` so the two match."
-  ))
-}
-
-if (!identical(exclude_hooks, read_config(here::here("inst/pre-commit-config-proj.yaml")))) {
-  rlang::abort(paste0(
-    "The `exclude` key from the spell-check hook should be copied from `.pre-commit-hooks.yaml` to ",
-    "`inst/pre-commit-config-proj.yaml` so the two match."
+    "The `exclude` key from the spell-check hook should be the same for all templates in `inst/`."
   ))
 }
