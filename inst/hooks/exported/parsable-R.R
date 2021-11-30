f1cc91cade86df67c080e80658c4c008a1c97069
Paul Wildenhain <wildenhaip@chop.edu>
2021-11-30 11:27:16 -0500

    :recycle: Add Rmds to parsable-R hook

--- inst/hooks/exported/parsable-R.R
+++ inst/hooks/exported/parsable-R.R
@@ -2,6 +2,17 @@
 files <- commandArgs(trailing = TRUE)
 
 out <- lapply(files, function(path) {
+  is_rmd <- grepl("\\.[rR]md$", path)
+  if (is_rmd) {
+    dir <- tempdir()
+    path <- knitr::purl(
+      input = path,
+      output = paste0(dir, "/file.R"),
+      quiet = TRUE,
+      documentation = FALSE
+    )
+  }
+
   tryCatch(
     parse(path),
     error = function(x) stop("File ", path, " is not parsable", call. = FALSE)
