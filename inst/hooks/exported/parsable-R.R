ca50a47fdb856af1be37928875db686628ef2ca9
Lorenz Walthert <lorenz.walthert@icloud.com>
2021-12-11 00:00:30 +0100

    make sure to emit origiinal path, not tempfile path

--- inst/hooks/exported/parsable-R.R
+++ inst/hooks/exported/parsable-R.R
@@ -4,16 +4,18 @@ files <- commandArgs(trailing = TRUE)
 out <- lapply(files, function(path) {
   is_rmd <- grepl("\\.[rR]md$", path)
   if (is_rmd) {
-    path <- knitr::purl(
+    path_ <- knitr::purl(
       input = path,
       output = tempfile(fileext = ".R"),
       quiet = TRUE,
       documentation = FALSE
     )
+  } else {
+    path_ <- path
   }
 
   tryCatch(
-    parse(path),
+    parse(path_),
     error = function(x) stop("File ", path, " is not parsable", call. = FALSE)
   )
 })
