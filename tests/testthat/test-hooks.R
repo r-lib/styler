f487e36affafe63688ab0291d627227387314563
Lorenz Walthert <lorenz.walthert@icloud.com>
2022-04-11 22:44:22 +0200

    any version of {styler} should now support ignore markers

--- tests/testthat/test-hooks.R
+++ tests/testthat/test-hooks.R
@@ -76,27 +76,22 @@ run_test("style-files",
   cmd_args = c("--include_roxygen_examples=FALSE", "--cache-root=styler")
 )
 
-if (rlang::is_installed("styler") && packageVersion("styler") > "1.6.2") {
-  if (Sys.Date() > as.Date("2022-0909")) {
-    rlang::abort("remove check, {styler} must have been upgraded on CRAN.")
-  }
-  run_test("style-files",
-    file_name = "style-files",
-    suffix = "-ignore-success.R",
-    cmd_args = c(
-      "--cache-root=styler",
-      '--ignore-start="^# styler: off$"',
-      '--ignore-stop="^# styler: on$"'
-    )
+run_test("style-files",
+  file_name = "style-files",
+  suffix = "-ignore-success.R",
+  cmd_args = c(
+    "--cache-root=styler",
+    '--ignore-start="^# styler: off$"',
+    '--ignore-stop="^# styler: on$"'
   )
+)
 
-  run_test("style-files",
-    file_name = "style-files",
-    suffix = "-ignore-fail.R",
-    cmd_args = "--cache-root=styler",
-    error_msg = "Invalid stylerignore sequences"
-  )
-}
+run_test("style-files",
+  file_name = "style-files",
+  suffix = "-ignore-fail.R",
+  cmd_args = "--cache-root=styler",
+  error_msg = "Invalid stylerignore sequences"
+)
 
 
 # fail with cmd args
