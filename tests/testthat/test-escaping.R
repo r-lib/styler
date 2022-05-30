f1d87bdbce5300316b4acc7b5b2a270b840eb535
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-05-30 22:49:22 +0200

    use snapshots for error
    @lorenzwalthert Can you please check if this is what you want?
    
    More specifically, do you want to include snaphots of entire error messages or are you happy with the current approach of using regexps to check error messages?

--- tests/testthat/test-escaping.R
+++ tests/testthat/test-escaping.R
@@ -3,19 +3,19 @@ test_that("escaping of characters works", {
     transformer = style_text
   ), NA)
 
-  expect_error(test_collection("escaping", "fail-parsing-1",
-    transformer = style_text
-  ), "<text>:2:7: unexpected ")
+  expect_snapshot_error(
+    test_collection("escaping", "fail-parsing-1", transformer = style_text)
+  )
 
-  expect_error(test_collection("escaping", "fail-parsing-2",
-    transformer = style_text
-  ), "x <-")
+  expect_snapshot_error(
+    test_collection("escaping", "fail-parsing-2", transformer = style_text)
+  )
 
-  expect_error(test_collection("escaping", "fail-parsing-3",
-    transformer = style_text
-  ))
+  expect_snapshot_error(
+    test_collection("escaping", "fail-parsing-3", transformer = style_text)
+  )
 
-  expect_error(test_collection("escaping", "fail-parsing-4",
-    transformer = style_text
-  ), "<text>:6:0:")
+  expect_snapshot_error(
+    test_collection("escaping", "fail-parsing-4", transformer = style_text)
+  )
 })
