01a6f31d853798e88433fe000aea1166b22c688a
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-09-13 09:28:21 +0200

    better formatting

--- src/ensure_last_n_empty.cpp
+++ src/ensure_last_n_empty.cpp
@@ -16,8 +16,7 @@ std::vector<std::string> ensure_last_n_empty(std::vector<std::string> x, int n =
 
   // Return early if all elements are empty
   // There should be only single empty element, no matter the value of `n`
-  if (std::all_of(x.begin(), x.end(), [](std::string s)
-  { return s == ""; }))
+  if (std::all_of(x.begin(), x.end(), [](std::string s) { return s == ""; }))
   {
     std::vector<std::string> x{""};
     return x;
@@ -30,15 +29,8 @@ std::vector<std::string> ensure_last_n_empty(std::vector<std::string> x, int n =
     return x;
   }
 
-  // If one or many empty elements are present at the end of x, remove them and add n of ""
-  if (x.back() == "")
-  {
-    auto it = std::find_if(x.rbegin(), x.rend(), [](std::string s)
-    { return s != ""; });
-    x.erase(it.base(), x.end());
-    std::fill_n(std::back_inserter(x), n, "");
-    return x;
-  }
-
+  auto it = std::find_if(x.rbegin(), x.rend(), [](std::string s) { return s != ""; });
+  x.erase(it.base(), x.end());
+  std::fill_n(std::back_inserter(x), n, "");
   return x;
 }
