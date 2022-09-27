035de78d40ce7f80ce642fece2584f34077ea04d
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-09-27 07:40:15 +0200

    Add vctrs to DESCRIPTION

--- R/utils.R
+++ R/utils.R
@@ -4,10 +4,12 @@ line_col_names <- function() {
   c("line1", "line2", "col1", "col2")
 }
 
+#' @keywords internal
 styler_df <- function(..., .size = NULL, .name_repair = "minimal") {
   vctrs::data_frame(..., .size = .size, .name_repair = .name_repair)
 }
 
+#' @keywords internal
 new_styler_df <- function(x) {
   vctrs::new_data_frame(x)
 }
