8895482344973e55cc3a53aca09f9ad2d65b17c9
Lorenz Walthert <lorenz.walthert@icloud.com>
2022-10-16 15:56:40 +0200

    avoid re-running

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,5 +5,4 @@ test_check("styler") # checks multiple files, in parallel
 # checks file one by one, not parallel
 Sys.setenv(STYLER_TEST_IS_TRULY_PARALLEL = FALSE)
 test_file("testthat/test-cache-high-level-api.R")
-test_file("testthat/test-cache-interaction-roxygen-code-examples.R")
 test_file("testthat/tests-cache-require-serial.R")
