2554221f86ea0608e2afa38470fcf7ff852e55ec
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-10-25 12:16:55 +0200

    fix example

--- R/expr-is.R
+++ R/expr-is.R
@@ -39,7 +39,11 @@ is_while_expr <- function(pd) {
 #' @examples
 #' \donttest{
 #' code <- "x <- list(1:3)"
-#' pd <- styler:::compute_parse_data_nested(code)
+#' pd <- styler:::compute_parse_data_nested(
+#'   code,
+#'   transformers = tidyverse_style(),
+#'   more_specs = NULL
+#' )
 #'
 #' is_function_call(pd)
 #'
