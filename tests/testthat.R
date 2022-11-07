c2e0d9ab67a06189cb86aaadd46bc098afe0e061
Lorenz Walthert <lorenz.walthert@icloud.com>
2022-11-07 19:57:23 +0100

    Don't run any tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,8 @@
-library(testthat)
-library(styler)
-test_check("styler") # checks multiple files, in parallel
-
-# checks file one by one, not parallel
-Sys.setenv(STYLER_TEST_IS_TRULY_PARALLEL = FALSE)
-test_file("testthat/test-cache-high-level-api.R")
-test_file("testthat/tests-cache-require-serial.R")
+# library(testthat)
+# library(styler)
+# test_check("styler") # checks multiple files, in parallel
+#
+# # checks file one by one, not parallel
+# Sys.setenv(STYLER_TEST_IS_TRULY_PARALLEL = FALSE)
+# test_file("testthat/test-cache-high-level-api.R")
+# test_file("testthat/tests-cache-require-serial.R")
