93c60f7ca04a4caa7755bda1e97d4fb525024cac
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-12-26 17:03:37 +0100

    rearrange list for readability

--- R/transform-block.R
+++ R/transform-block.R
@@ -1,7 +1,7 @@
 #' Parse, transform and serialize a nested parse table
 #'
 #' We process blocks of nested parse tables for speed. See [cache_find_block()]
-#' for details on how a top level nest is split into blocks.
+#' for details on how a top-level nest is split into blocks.
 #' @param pd_nested A block of the nested parse table.
 #' @param start_line The line number on which the code starts.
 #' @param base_indention Integer scalar indicating by how many spaces the whole
@@ -56,29 +56,35 @@ parse_transform_serialize_r_block <- function(pd_nested,
 
 #' Find the groups of expressions that should be processed together
 #'
-#' @param pd A top level parse table.
+#' @param pd A top-level parse table.
+#'
 #' @details
+#'
 #' We want blocks to be formed according to these rules:
-#' - blocks should contain either cached or uncached expressions only. If a
-#'   block contains cached expressions only, it does not have to be processed
-#'   and can be returned immediately. If a block contains uncached expressions,
-#'   it makes sense to put as many uncached expression in it, since processing
-#'   one bigger block has less overhead than processing many smaller blocks.
-#' - Multiple expressions can sit on one row, e.g. in line comment and commands
-#'   separated with ";". This creates a problem when processing each expression
-#'   separately because when putting them together, we need complicated handling
-#'   of line breaks between them, as it is not apriori clear that there is a
-#'   line break separating them. To avoid this, we put top level expressions
-#'   that sit on the same line into one block, so the assumption that there is a
-#'   line break between each block of expressions holds.
-#' - all expressions in a stylerignore sequence must be in the same block. If
-#'   that's not the case, the first expression in a block might not be a
-#'   top-level terminal, but another top-level expression.
-#'   [apply_stylerignore()]  joins `env_current$stylerignore`, which contains
-#'   only terminals, with the first expression in a stylerignore sequence, based
-#'   on the first `pos_id` in that stylerignore sequence
-#'   (`first_pos_id_in_segment`).
-#' @param pd A top level nest.
+#'
+#'  - Blocks should contain either cached or uncached expressions only. If a
+#'    block contains cached expressions only, it does not have to be processed
+#'    and can be returned immediately. If a block contains uncached expressions,
+#'    it makes sense to put as many uncached expression in it, since processing
+#'    one bigger block has less overhead than processing many smaller blocks.
+#'
+#'  - Multiple expressions can sit on one row, e.g. in-line comment and commands
+#'    separated with ";". This creates a problem when processing each expression
+#'    separately because when putting them together, we need complicated handling
+#'    of line breaks between them, as it is not *a priori* clear that there is a
+#'    line break separating them. To avoid this, we put top-level expressions
+#'    that sit on the same line into one block, so the assumption that there is a
+#'    line break between each block of expressions holds.
+#'
+#'  - All expressions in a stylerignore sequence must be in the same block. If
+#'    that's not the case, the first expression in a block might not be a
+#'    top-level terminal, but another top-level expression.
+#'    [apply_stylerignore()]  joins `env_current$stylerignore`, which contains
+#'    only terminals, with the first expression in a stylerignore sequence, based
+#'    on the first `pos_id` in that stylerignore sequence
+#'    (`first_pos_id_in_segment`).
+#'
+#' @param pd A top-level nest.
 #' @keywords internal
 cache_find_block <- function(pd) {
   first_after_cache_state_switch <- pd$is_cached != lag(pd$is_cached, default = !pd$is_cached[1L])
@@ -123,7 +129,7 @@ find_blank_lines_to_next_expr <- function(pd_nested) {
 #' blank lines between them. Note that because code does not need to start on
 #' line 1, the first element of the output is the number of lines until the
 #' first block.
-#' @param pd A top level nest.
+#' @param pd A top-level nest.
 #' @keywords internal
 find_blank_lines_to_next_block <- function(pd) {
   block_boundary <- pd$block != lag(pd$block, default = 0L)
