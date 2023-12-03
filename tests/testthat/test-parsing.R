dc606ef9c03ba8c96df42e0a64c7e81f721ed453
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-12-03 21:51:51 +0100

    Remove outdated test about repeated parsing
    cf. #1161

--- tests/testthat/test-parsing.R
+++ tests/testthat/test-parsing.R
@@ -1,30 +1,3 @@
-test_that("repreated parsing solves wrong parent assignment", {
-  expect_warning(
-    test_collection(
-      "parsing", "repeated_parsing",
-      transformer = style_text,
-      strict = FALSE
-    ),
-    NA
-  )
-
-  # move to temp dir
-  dir <- tempfile("styler")
-  dir.create(dir)
-  path_temp <- file.path(dir, "repeated_parsing-in.R")
-  path_perm <- testthat_file("parsing", "repeated_parsing-in.R")
-  file.copy(path_perm, dir)
-
-  sys_call <- paste0(
-    "R -q -e \"styler::cache_deactivate(); styler::style_file(\\\"", path_temp, "\\\")\""
-  )
-  calls_sys(sys_call, intern = FALSE, ignore.stdout = TRUE, ignore.stderr = TRUE)
-  ref <- read_utf8_bare(testthat_file("parsing", "repeated_parsing-out.R"))
-  result <- read_utf8_bare(path_temp)
-  expect_equal(ref, result)
-  unlink(dir)
-})
-
 test_that("long strings are parsed correctly", {
   expect_warning(
     test_collection("parsing", "long_strings", transformer = style_text),
