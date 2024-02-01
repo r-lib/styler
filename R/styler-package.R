2bb93a454d5917296076305d8e09d41aa5047efa
olivroy <olivierroy71@hotmail.com>
2024-02-01 08:34:33 -0500

    Cosmetic change of package doc

--- R/styler-package.R
+++ R/styler-package.R
@@ -1,14 +1,13 @@
-#' Non-invasive pretty printing of R code
-#'
-#' styler allows you to format `.R`, `.Rmd`, `.Rmarkdown` and/or
-#' `.qmd`, `.Rnw` files, R packages, or entire R source trees
+#' @description
+#' styler allows you to format `.qmd`, `.R`, `.Rmd`, `.Rmarkdown`,
+#' `.Rnw`, and/or `.Rprofile` files, R packages, or entire R source trees
 #' according to a style guide.
 #' The following functions can be used for styling:
 #' * [style_text()] to style a character vector.
 #' * [style_file()] to style a single file.
 #' * [style_dir()] to style all files in a directory.
 #' * [style_pkg()] to style the source files of an R package.
-#' * [styler_addins] (RStudio Addins) to style either selected code or the
+#' * [RStudio Addins][styler_addins] to style either selected code or the
 #' active file.
 #' @examples
 #' style_text("call( 1)")
