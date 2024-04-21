b1d08155a354917fac85ad82eb2053eb8b066a3c
Indrajeet Patil <patilindrajeet.science@gmail.com>
2024-04-21 10:15:26 +0200

    also include defaults

--- R/options.R
+++ R/options.R
@@ -21,14 +21,36 @@
 #'
 #' @section Options for the styler package:
 #'
-#' - `styler.addins_style_transformer`: character. The name of the style transformer to use in the addins.
-#' - `styler.cache_name`: character. The name of the styler cache to use.
-#' - `styler.cache_root`: character. The directory where the cache files are stored.
-#' - `styler.colored_print.vertical`: logical. It decides whether or not the output should be colored with
-#'   `prettycode::highlight()`.
-#' - `styler.ignore_alignment`: logical. If `TRUE`, alignment, when detected, is ignored.
-#' - `styler.ignore_start`, `styler.ignore_stop`: character. Regular expressions to ignore lines that match them.
-#' - `styler.quiet`: logical. It decides whether or not to print an informative message about what the
-#'   function is doing. If `TRUE`, no output is printed.
-#' - `styler.test_dir_writable`: logical. If `TRUE`, the package tests whether the directory is writable.
+#' - `styler.addins_style_transformer`: character.
+#'   The name of the style transformer to use in the addins.
+#'   (default: `"styler::tidyverse_style()"`)
+#'
+#' - `styler.cache_name`: character.
+#'   The name of the styler cache to use.
+#'   (default: current styler package version)
+#'
+#' - `styler.cache_root`: character.
+#'   The directory where the cache files are stored.
+#'   (default: `NULL`)
+#'
+#' - `styler.colored_print.vertical`: logical.
+#'   It decides whether or not the output should be colored with `prettycode::highlight()`.
+#'   (default: `TRUE`)
+#'
+#' - `styler.ignore_alignment`: logical.
+#'   If `TRUE`, alignment, when detected, is ignored.
+#'   (default: `FALSE`)
+#'
+#' - `styler.ignore_start`, `styler.ignore_stop`: character.
+#'   Regular expressions to ignore lines that match them.
+#'   (default: `"styler: off"` and `"styler: on"`, respectively)
+#'
+#' - `styler.quiet`: logical.
+#'   It decides whether or not to print an informative message about what the function is doing.
+#'   If `TRUE`, no output is printed.
+#'   (default: `FALSE`)
+#'
+#' - `styler.test_dir_writable`: logical.
+#'   If `TRUE`, the package tests whether the directory is writable.
+#'   (default: `TRUE`)
 NULL
